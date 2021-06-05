2781e8fffe518be4f7e38c4f5d21c37ee5649fee
Brian Ramsay <ramsayb2@berea.edu>
2021-06-04 23:15:34 -0400

    Get configuration in order

--- app/__init__.py
+++ app/__init__.py
@@ -1,46 +1,38 @@
 from flask import Flask
+import os
 import yaml
-from flask_bootstrap import Bootstrap
-from flask_nav import Nav
-from flask_nav.elements import *
 
-
+# Initialize our application
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
-# login = LoginManager(app)  #FIXME: needs configured with our dev/prod environment handlers
 
-def load_config(file):
-    with open(file, 'r') as ymlfile:
-        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-    return cfg
+######### Set up App Configuration #############
+# Uses config2 - https://pypi.org/project/config2/ - with the addition of an uncommitted 
+# override yml to set instance parameters. By default, 'local-override.yml'
+#
+# Precedence of configuration values is as follows:
+# 
+# local-override.yml
+#     ↓
+# environment file (e.g., development.yml, production.yml)
+#     ↓
+# default.yml
+#
+################################################
 
-cfg = load_config("app/config/secret_config.yaml")
-app.secret_key = cfg["secret_key"]
+# ensure ENV matches flask environment (for config2)
+os.environ["ENV"] = app.get_env()
 
-app.config['use_shibboleth'] = False
-if app.config['ENV'] == 'production':
-    app.config['use_shibboleth'] = True
+app.config.update(config.get())
+override_path = "app/config/" + app.config.override_file
+try:
+    with open(override_path, 'r') as ymlfile:
+        app.config.update(yaml.load(ymlfile, Loader=yaml.FullLoader))
+except FileNotFoundError e:
+    Path(override_path).touch() # create it if it doesn't exist
 
-app.config['use_tracy'] = False
-if app.config['ENV'] in ('production','staging'):
-    app.config['use_tracy'] = True
+app.secret_key = app.config['secret_key']
 
-app.config['use_banner'] = False
-if app.config['ENV'] in ('production','staging'):
-    app.config['use_banner'] = True
-
-# Registers blueprints (controllers). These are general routes, like /index
-from app.controllers.main_routes import main_bp as main_bp
-app.register_blueprint(main_bp)
-
-# Registers the admin interface blueprints
-from app.controllers.admin_routes import admin as admin_bp
-app.register_blueprint(admin_bp)
-
-# Registers error messaging
-from app.controllers.errors_routes import error as errors_bp
-app.register_blueprint(errors_bp)
-
+# Make 'env' a variable everywhere
 @app.context_processor
 def inject_environment():
-    return dict(env=app.config['ENV'])
+    return dict(env=app.get_env())
