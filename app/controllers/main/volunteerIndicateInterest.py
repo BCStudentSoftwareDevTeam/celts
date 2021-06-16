8b87336ee3212522f828dfd4d8d52f715c52b757
escalerapadronl <escalerapadronl@berea.edu>
2021-06-16 14:07:09 -0400

    completed conditional in controller that updates Interest table when user checks or unchecks a checkbox.

--- app/controllers/main/volunteerIndicateInterest.py
+++ app/controllers/main/volunteerIndicateInterest.py
@@ -16,16 +16,21 @@ def volunteerIndicateInterest():
                            programs = programs)
 
 
-@main_bp.route('/updateInterest/<program_id>/<interest>', methods = ['POST'])
-def updateInterest(program_id, interest):
+@main_bp.route('/updateInterest/<program_id>/<num_interest>', methods = ['POST'])
+def updateInterest(program_id, num_interest):
     """
     This function updates the interest table by adding a new row when a user
     shows interest in a program or by deleting the row from the table when a
     user no longer shows interest in a program.
     """
-    if interest:
-        Interest.create(program_id, current_user.username)
+    print(type(num_interest))
+    num_interest = int(num_interest)
+    if num_interest:
+        print("Stepped into if")
+        Interest.get_or_create(program = program_id, user = current_user.username)
     else:
+        print("stepped into else")
         deleted_interest = Interest.get(Interest.program == program_id and Interest.user == current_user.username)
+        print(deleted_interest)
         deleted_interest.delete_instance()
     return jsonify({"Success": False})
--- app/static/js/volunteerIndicateInterest.js
+++ app/static/js/volunteerIndicateInterest.js
@@ -1,13 +1,15 @@
 function updateInterest(el){
   //create bool var storing interest or no interest
   var interest = $(el).is(':checked');
+  numInterest = Number(interest)
+  console.log(numInterest);
   //create var with program id
   var programID = $(el).attr('id');
   //create ajax call with data
   $.ajax({
     method: "POST",
-    url: "/updateInterest/" + programID + '/' + interest,
-    data: programID, interest,
+    url: "/updateInterest/" + programID + '/' + numInterest,
+    data: programID, numInterest,
     success: function(response) {
       if (response) {
         console.log('YAY YOU MADE IT BACK');
