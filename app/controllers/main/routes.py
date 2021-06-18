ed23045733e41bdad8c6087f77584c36ffdbdd91
Will Romano <romanow@berea.edu>
2021-06-18 13:46:20 -0400

    Copied all files over from local directory and restructured filesystem. Everything is running without bugs

--- app/controllers/main/routes.py
+++ app/controllers/main/routes.py
@@ -3,5 +3,10 @@ from flask import request, render_template
 from app.controllers.main import main_bp
 
 @main_bp.route('/')
-def home():
-   return render_template('home.html', title="Welcome to CELTS!") 
+def index():
+    try:
+        return render_template('main/index.html', title="Welcome to CELTS!")
+    except Exception as e:
+        #TODO We have to return some sort of error page
+        print('Error in main page:', e)
+        return "",500
