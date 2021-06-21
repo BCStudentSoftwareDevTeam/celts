783f4b882bb8e90af5ec832af09fdaf1727f3cca
None <agliullovak@ssdt2021agliullovak.ssdt.berea.edu>
2021-06-21 14:39:37 -0400

    Added docstrings, modified the code to make it more organized

--- app/controllers/events/meetsReqsForEvent.py
+++ app/controllers/events/meetsReqsForEvent.py
@@ -5,10 +5,19 @@ from app.models.programBan import ProgramBan
 from app.models.preqForProgram import PreqForProgram
 
 def isEligibleForProgram(event, user):
+    """
+    Checks if the user is banned from an event. Checks if the user has completed
+    specific prerequisite for the event.
+    :param event: the name of the event that is passed in
+    :param user: the user that is logged into the system
+    :return: True if the user is not banned and meets the requirements, and False otherwise
+    """
 
-    program = Event.select(Event.program).where(Event.id == event)  # assuming that the event belongs to one program
+    program = Event.select(Event.program).where(Event.id == event)
+    # assuming that the event belongs to one program
 
-    if (ProgramBan.select().where(ProgramBan.user == user)) and (ProgramBan.select().where(ProgramBan.program == program)):
+    if (ProgramBan.select().where(ProgramBan.user == user)) and
+    (ProgramBan.select().where(ProgramBan.program == program)):
         return False
 
     for requirement in PreqForProgram.select().where(PreqForProgram.event == event):
--- app/controllers/events/volunteerRegisterEvent.py
+++ app/controllers/events/volunteerRegisterEvent.py
@@ -6,14 +6,26 @@ from app.controllers.events.meetsReqsForEvent import isEligibleForProgram
 import pytest
 
 def volunteerRegister(userid,  eventid):
+    """
+    Lets the user register for an event if they are eligible
+    :param userid: indicates the id that the user has
+    :param eventid: indicates the id that the event has
+    :return: a message that indicates what the user has registered for if eligible;
+    otherwise lets the user know that they are not eligible for registration
+    """
 
     user = User.get(User.username == userid)
     event = Event.get(Event.id == eventid)
-    # Assuming the student meets the requirement for the events (function wriiten by Zach and KArina)
+    # Assuming the student meets the requirement for the events (function wriiten
+    # by Zach and KArina)
 
     if isEligibleForProgram(event, user):
-        if EventParticipant.select().where(EventParticipant.user == user, EventParticipant.event == event) is None:
-            eventParticipant = EventParticipant.create(user = user, event = event, rsvp = True)
+        if EventParticipant.select()
+                           .where(EventParticipant.user == user,
+                                  EventParticipant.event == event) is None:
+            eventParticipant = EventParticipant.create(user = user,
+                                                       event = event,
+                                                       rsvp = True)
         else:
             return f"{user} is already registered for {event}"
         return eventParticipant
