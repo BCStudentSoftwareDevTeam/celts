acf31c76820b47ca2464858571b80f6fe6d61041
None <jarjug@ssdt2021jarjug.ssdt.berea.edu>
2021-06-21 15:36:37 -0400

    Removed the print statements we had for testing

--- app/static/js/volunteerIndicateInterest.js
+++ app/static/js/volunteerIndicateInterest.js
@@ -1,14 +1,9 @@
 $(document).ready(function() {
 $(".form-check-input").click(function updateInterest(el){
-  //create bool var storing interest or no interest
   var programID = $(this).attr('id');
   var interest = $(this).is(':checked');
   var userID = $("#escalerapadronl").attr("id")
-  console.log(programID)
-  //create ajax call with data
-  console.log("Enter Function");
   if (interest) {
-    console.log("Enter IF");
   $.ajax({
     method: "POST",
     url: "/addInterest/" + programID + '/' + userID,
@@ -17,7 +12,6 @@ $(".form-check-input").click(function updateInterest(el){
       if (response) {
         $("#flasher-container").prepend('<div class="alert" id="flasher" data-dismiss="alert" role="alert">This is a success alert—check it out!</div>');
         $("#flasher").delay(3000).fadeOut();
-
       }
     },
     error: function(request, status, error) {
@@ -26,7 +20,6 @@ $(".form-check-input").click(function updateInterest(el){
   });
   }
   else{
-    console.log("Enter ELSE");
   $.ajax({
     method: "POST",
     url: "/deleteInterest/" + programID + '/' + userID,
@@ -35,7 +28,6 @@ $(".form-check-input").click(function updateInterest(el){
       if (response) {
         $("#flasher-container").prepend('<div class="alert" id="flasher" data-dismiss="alert" role="alert">This is a success alert—check it out!</div>');
         $("#flasher").delay(3000).fadeOut();
-
       }
     },
     error: function(request, status, error) {
