dde0a21d2752dd913369975ada45560b737d01f0
None <partont@ssdt2021partont.ssdt.berea.edu>
2021-06-23 09:18:17 -0400

    Got a rough version of the search bars working.

--- app/static/js/addParticipants.js
+++ app/static/js/addParticipants.js
@@ -10,3 +10,34 @@ var row = btn.parentNode.parentNode;
 row.parentNode.removeChild(row);
 
 }
+
+
+function searchParticipants() {
+    let input = document.getElementById('Outsearch').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('outsidepart');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";
+        }
+    }
+}
+
+function searchVolunteers() {
+    let input = document.getElementById('Volsearch').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('Volunteers');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";
+        }
+    }
+}
