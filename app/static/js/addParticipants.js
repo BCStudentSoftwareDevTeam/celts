afbb45b4da55523d3d977a8c5af7eb928a087aaf
None <partont@ssdt2021partont.ssdt.berea.edu>
2021-06-24 08:45:25 -0400

    Updated branch with development, created a modal with the correct form.

--- app/static/js/addParticipants.js
+++ app/static/js/addParticipants.js
@@ -6,49 +6,23 @@ $( document ).ready(function() {
 
 function removeParticipants(btn) {
 
-  var row = btn.parentNode.parentNode;
-  row.parentNode.removeChild(row);
+  var row = btn.parent().parent();
+  row.parent().removeChild(row);
 }
 
 function removeVolunteer(btn) {
 
-  var row = btn.parentNode.parentNode;
-  row.parentNode.removeChild(row);
+  var row = btn.parent().parent();
+  row.parent().removeChild(row);
 
-}
-
-function searchParticipants() {
-    let input = $('#Outsearch').val()
-    input = input.toLowerCase();
-    let x = $('.outsidepart');
-    for (i = 0; i < x.length; i++) {
-        if (!$('.outsidepart').index().html().toLowerCase().includes(input) || !input) {
-            $(".outsidepart").hide();
-        }
-        else {
-          $(".outsidepart").show();
-        }
-    }
-}
-
-function searchVolunteers() {
-    let input = $('#Volsearch').val()
-    input = input.toLowerCase();
-    let x = $('.Volunteers');
-    for (i = 0; i < x.length; i++) {
-        if (!x[i].html().toLowerCase().includes(input) || !input) {
-            $(".Volunteers").hide();
-        }
-        else {
-            $(".Volunteers").show();
-        }
-    }
 }
 
 function textboxValue() {
-firstName = $("#firstNameTextarea").value;
-lastName = $("#lastNameTextarea").value;
-emailEntry = $("#emailTextarea").value;
-phoneNumber = $("#phoneNumberTextarea").value;
+firstName = $("#firstNameTextarea").val();
+lastName = $("#lastNameTextarea").val();
+emailEntry = $("#emailTextarea").val();
+phoneNumber = $("#phoneNumberTextarea").val();
+console.log(firstName)
+console.log(phoneNumber)
 // $("#OutsideTable").append('<tr><td>{{ firstName + "" + lastName }}</td><td>{{ emailEntry }}</td> <td>{{ phoneNumber }}</td><td><button onclick="removeParticipants(this)" type="button">x</button></td></tr>');
 }
