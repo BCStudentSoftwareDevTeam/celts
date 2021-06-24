c86c2e8956c59351181eb507d313b4dae2686463
None <jarjug@ssdt2021jarjug.ssdt.berea.edu>
2021-06-24 15:15:07 -0400

    Made textboxes reset back to placeholder text and fix the search bar so that user's name can pop up when searched.

--- app/static/js/addParticipants.js
+++ app/static/js/addParticipants.js
@@ -1,8 +1,35 @@
 $( document ).ready(function() {
     $(".Volunteers").hide();
     $(".outsidepart").hide();
-
 });
+
+$(document).ready(function(){
+  $("#Outsearch").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#Partul li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      if(!value) {
+        $(".outsidepart").hide();
+      }
+      });
+    });
+  });
+
+$(document).ready(function(){
+  $("#Volsearch").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#Volul li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      if(!value) {
+        $(".Volunteers").hide();
+      }
+    });
+  });
+});
+
+function addResult(){
+  console.log("Added!")
+}
 
 function removeParticipants(e) {
 
@@ -20,4 +47,8 @@ lastName = $("#lastNameTextarea").val();
 emailEntry = $("#emailTextarea").val();
 phoneNumber = $("#phoneNumberTextarea").val();
 $("#OutsideTable").append('<tr id="removeRow"><td>' + firstName + " " + lastName + " " + '</td><td>' + emailEntry + " " +'</td><td>' + phoneNumber + " " + '</td><td><button id="removeButton" onclick="removeParticipants(this)" type="button">x</button></td></tr>');
+$('#firstNameTextarea').val('').blur();
+$('#lastNameTextarea').val('').blur();
+$('#emailTextarea').val('').blur();
+$('#phoneNumberTextarea').val('').blur();
 }
