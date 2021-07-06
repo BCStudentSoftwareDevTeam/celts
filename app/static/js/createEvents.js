f6a8ba8d5f3141bc54b40db3c41750016ec1a70a
Sandesh Lamichhane <lamichhanes2@berea.edu>
2021-07-06 15:15:21 -0400

    removed some commented out code

--- app/static/js/createEvents.js
+++ app/static/js/createEvents.js
@@ -1,12 +1,4 @@
 
-// function toggleEndDate(){
-//   if ($('input[name="recurringEvent"]:checked').val() == "true"){
-//     $("#endDateStyle").removeClass("d-none");
-//   }else{
-//     $("#endDateStyle").addClass("d-none");
-//   }
-// }
-
 $(document).ready(function(){
 
   $(".readonly").on('keydown paste', function(e){ //makes the input fields act like readonly (readonly doesn't work with required)
@@ -78,62 +70,3 @@ function dragOverHandler(ev) {
   // Prevent default behavior (Prevent file from being opened)
   ev.preventDefault();
 }
-
-
-// function createEventValuesDict(){
-//   var eventName = $("#inputEventName").val();
-//   var term = $("#inputEventTerm").find("option:selected").attr("value");
-//   var recurringEvents= $('input[name="recurringEvent"]:checked').val();
-//   var startDate = $("#startDatePicker").datepicker("option", "dateFormat", "yy-mm-dd" ).val();
-//   var endDate =  $("#endDatePicker").datepicker("option", "dateFormat", "yy-mm-dd" ).val();
-//   var startTime = $("#pickStartTime").val();
-//   var endTime = $("#pickEndTime").val();
-//   var location = $("#inputEventLocation").val();
-//   var requiredForProgram = $("#checkIsRequired").is(":checked");
-//   var requireForRSVP = $("#rsvp").is(":checked");
-//   var serviceHours = $("#earnServiceHours").is(":checked");
-//   var description = $("#inputEventDescription").val();
-//   var facilitators = $("#inputEventFacilitators").val();
-//
-//   var eventDict = {evName: eventName,
-//                    evTerm: term,
-//                    evRecurringEvent: recurringEvents,
-//                    evStartDate: startDate,
-//                    evEndDate: endDate,
-//                    evStartTime: startTime,
-//                    evEndTime: endTime,
-//                    evLocation: location,
-//                    evRequiredForProgram: requiredForProgram,
-//                    evRSVP: requireForRSVP,
-//                    evServiceHours: serviceHours,
-//                    evDescription: description,
-//                    evFacilitators: facilitators
-//   }
-//   return eventDict;
-// }
-
-
-
-// function createNewEvent(){
-//
-//
-//       events = createEventValuesDict()
-//       var data = JSON.stringify(events);
-//       console.log(data)
-//       $.ajax({
-//        method: "POST",
-//        url: '/createEvents',
-//        contentType: "application/json",
-//        dataType: "text",
-//        data: data,
-//        success: function(result) {
-//          alert(result)
-//          console.log(result)
-//        },
-//        error: function(xhr, status, error){
-//          alert("Something went wrong! " + String(error));
-//          console.log("Something went wrong!")
-//        }
-//       });
-//
-// }
