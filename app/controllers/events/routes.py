6f36dc0c7c7d1b29276e8408dda5937d69226c9d
None <ayisie@ssdt2021ayisie.ssdt.berea.edu>
2021-07-07 13:48:59 -0400

    made routes for view all events

--- app/controllers/events/routes.py
+++ app/controllers/events/routes.py
@@ -1,14 +1,20 @@
 from flask import request, render_template
 from flask import Flask, redirect, flash
 
+from app.models.program import Program
 from app.controllers.events import events_bp
-from app.logic.events import getEvents
+from app.logic.events import groupingEvents
 
-@events_bp.route('/events', methods=['GET'])
-def events():
-    events = getEvents()
+@events_bp.route('/events/<term>', methods=['GET'])
+def events(term):
+
+    events = groupingEvents(term)
+    programs = Program.select()
+    for event in events:
+        print(event.program)
 
     return render_template("events/event_list.html",
             events=events,
+            programs = programs,
             user="ramsayb2"
             )
--- app/logic/events.py
+++ app/logic/events.py
@@ -9,3 +9,28 @@ def getEvents(program_id=None):
         return Event.select(Event).where(Event.program == program_id)
     else:
         return Event.select()
+
+def groupingEvents(termID):
+    # groupEvents = sorted(groupEvents, key = itemgetter('Event.program'))
+    #
+    # for key, value in groupby(groupEvents,key = itemgetter('Event.program')):
+    #     print(key)
+    #     for k in value:
+    #         print(k)
+    # return (key,k)
+
+    groupEvents = (Event.select(Event.description,Event.program).where(Event.term == termID).order_by(Event.program))
+    for item in list(groupEvents.objects()):
+        print(item.program)
+
+
+
+
+
+
+
+# events = [item for item in groupEvents.objects()]
+
+    #check if the program are the same
+    return groupEvents
+    # return [group for group in groupingEvents|compare(Event.program)]
