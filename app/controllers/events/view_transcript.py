ecfe3efa3eaa67ef4315016d17103826ac0c5fcc
Brian Ramsay <ramsayb2@berea.edu>
2021-07-08 15:09:32 -0400

    Fix PR comments

--- app/controllers/events/view_transcript.py
+++ app/controllers/events/view_transcript.py
@@ -6,67 +6,58 @@ from app.models.user import User
 from app.models.term import Term
 from app.models.eventParticipant import EventParticipant
 from app.models.event import Event
-from peewee import DoesNotExist
+from peewee import DoesNotExist, fn
 
-from peewee import *
-
-def getSlCourseTranscript(user):
+def getSlCourseTranscript(username):
     """
     Returns a service-learning course list that contains a name of the course, term,
     list of instructors who teach the course, and hours earned for each course that the user took.
     :user: model object
     """
-    username = User.get_by_id(user)
+    user = User.get_by_id(username)
     slCourseInformation = (CourseParticipant
-    .select(CourseParticipant.course, CourseParticipant.user, fn.SUM(CourseParticipant.hoursEarned).alias("hoursEarned"))
-    .group_by(CourseParticipant.course, CourseParticipant.user)
-    .where(CourseParticipant.user == username))
+        .select(CourseParticipant.course, CourseParticipant.user, fn.SUM(CourseParticipant.hoursEarned).alias("hoursEarned"))
+        .group_by(CourseParticipant.course, CourseParticipant.user)
+        .where(CourseParticipant.user == user))
 
     allCoursesList = []
-    listOfEachSlCourseInfo = []
-    
     for course in slCourseInformation:
-        user_full_name = course.user.firstName + " "+ course.user.lastName
+        user_full_name = f"{course.user.firstName} {course.user.lastName}"
         course_name = course.course.courseName
         description = course.course.term.description
         hours = course.hoursEarned
-        cInstructor = CourseInstructor.select().where(CourseInstructor.course== course.course)
-        instructorList = []
-        for instructor in cInstructor.objects():
-            instructorList.append(instructor.user.firstName+" "+ instructor.user.lastName)
+        instructorQuery = CourseInstructor.select().where(CourseInstructor.course == course.course)
+        instructorList = [f"{i.user.firstName} {i.user.lastName}" for i in instructorQuery]
+
         #creates a list for all information of a course that a courseParticipant is involved in
-        listOfEachSlCourseInfo = [ user_full_name , course_name , description , hours , instructorList ]
-        allCoursesList.append(listOfEachSlCourseInfo)
+        allCoursesList.append([ user_full_name , course_name , description , hours , instructorList ])
 
     return allCoursesList
 
 
-def getProgramTranscript(user):
+#FIXME: Needs to break hours down by program and term, not just program
+def getProgramTranscript(username):
     """
     Returns a list of programs that the user participated in. The list includes a program name,
     term, and hours earned for each program that the user attended.
     :user: model object
     """
-    username = User.get_by_id(user)
+    user = User.get_by_id(username)
     # Add up hours earned for each program they've participated in
     programInformation = (EventParticipant
         .select(EventParticipant.event , fn.SUM(EventParticipant.hoursEarned).alias("hoursEarned"))
-        .where(EventParticipant.user == username)
+        .where(EventParticipant.user == user)
         .join(Event)
         .join(Program)
         .group_by(EventParticipant.event.program))
 
     listOfProgramsTranscript = []
-    programTranscript = []
-
     for program in programInformation:
 
         programName = program.event.program.programName
-        #FIXME: If there are more than one events for the program and the term.description are different:
-            #term = "earliest term - final term"
         term = program.event.term.description
         totalHoursEarnedForProgram = program.hoursEarned
-        programTranscript = [ programName , term , totalHoursEarnedForProgram ]
-        listOfProgramsTranscript.append(programTranscript)
+
+        listOfProgramsTranscript.append([ programName , term , totalHoursEarnedForProgram ])
 
     return listOfProgramsTranscript
