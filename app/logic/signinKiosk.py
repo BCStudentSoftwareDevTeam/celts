75abb70d2edb47f7a8b1f4349c40ebcfbe6defbb
None <neillz@ssdt2021neillz.ssdt.berea.edu>
2021-07-16 12:18:08 -0400

    finished back end for kiosk sign in

--- app/logic/signinKiosk.py
+++ app/logic/signinKiosk.py
@@ -1,12 +1,19 @@
 
-from flask import Flask, flash
+from flask import Flask, flash, redirect
 from app.models.event import Event
+from app.models.eventParticipant import EventParticipant
 from app.models.user import User
 from app.controllers.events import events_bp
 from app.logic.events import getEvents
 
 
-
-@events_bp.route('/<eventid>/signintoKiosk', methods=['POST'])
-def signinKiosk(eventid):
-    print("")
+def sendkioskData(bnumber, eventid):
+    # event = Event.get_by_id(eventid)
+    bNumberToUser = User.select().where(User.bnumber == bnumber)
+    bNumberToUsername = bNumberToUser.objects()[0]
+    bNumbertoParticipant = (EventParticipant.update({EventParticipant.attended: True})
+                                         .where(EventParticipant.user == bNumberToUsername)
+                                         .where(EventParticipant.event == eventid))
+    bNumbertoParticipant.execute()
+    print("Passed execute")
+    return bNumberToUsername
--- app/static/js/eventKiosk.js
+++ app/static/js/eventKiosk.js
@@ -1,29 +1,29 @@
+function enterFullscreen() {
+  var el = document.documentElement
+    , rfs = // for newer Webkit and Firefox
+           el.requestFullscreen
+        || el.webkitRequestFullScreen
+        || el.mozRequestFullScreen
+        || el.msRequestFullscreen
+    ;
+  if(typeof rfs!="undefined" && rfs){
+    rfs.call(el);
+  } else if(typeof window.ActiveXObject!="undefined"){
+    // for Internet Explorer
+    var wscript = new ActiveXObject("WScript.Shell");
+    if (wscript!=null) {
+       wscript.SendKeys("{F11}");
+    }
+  }
+};
 
-// var elem = document.documentElement;
-$( document ).ready(function() {
-  var e = $.Event('keydown', { keyCode: 122 });
-    $(document).trigger(e);
-  //
-  //   if (!document.fullscreenElement &&    // alternative standard method
-  //     !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
-  //   if (document.documentElement.requestFullscreen) {
-  //     document.documentElement.requestFullscreen();
-  //   } else if (document.documentElement.msRequestFullscreen) {
-  //     document.documentElement.msRequestFullscreen();
-  //   } else if (document.documentElement.mozRequestFullScreen) {
-  //     document.documentElement.mozRequestFullScreen();
-  //   } else if (document.documentElement.webkitRequestFullscreen) {
-  //     document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-  //   }
-  // } else {
-  //   if (document.exitFullscreen) {
-  //     document.exitFullscreen();
-  //   } else if (document.msExitFullscreen) {
-  //     document.msExitFullscreen();
-  //   } else if (document.mozCancelFullScreen) {
-  //     document.mozCancelFullScreen();
-  //   } else if (document.webkitExitFullscreen) {
-  //     document.webkitExitFullscreen();
-  //   }
-  // }
+$('#submitScannerData').on('change', function() {
+  console.log("Worked");
+  $(this).closest('form').submit();
 });
+//
+// function submitData() {
+//   console.log("Created");
+//   $('#kioskForm').delay(200).submit();
+//   $('')
+// }
