8bbe07932e6af346bcc4f7d5f56655f754ea85f8
Gaston Jarju <jajug@berea.edu>
2021-07-22 16:14:19 -0400

    Created an html files and render the page.

--- app/controllers/admin/routes.py
+++ app/controllers/admin/routes.py
@@ -6,3 +6,7 @@ from app.controllers.admin import admin_bp
 @admin_bp.route('/testing', methods=['GET'])
 def testing():
     return "<h1>Hello</h1>"
+
+@admin_bp.route('/search_student', methods=['GET'])
+def studentSearchPage():
+    return render_template("/searchStudentPage.html")
