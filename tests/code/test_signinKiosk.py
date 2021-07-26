b4708eaeceae9f91e2ddc24e9ce4241ec636182b
Zach Neill <neillz@ssdt2021neillz.ssdt.berea.edu>
2021-07-26 10:01:07 -0400

    added to test file. IN PROGRESS, have yet to clean up data.

--- tests/code/test_signinKiosk.py
+++ tests/code/test_signinKiosk.py
@@ -1,13 +1,25 @@
 import pytest
-from app.logic.signinKiosk import sendkioskData
+from app.logic.signinKiosk import sendUserData
 from app.models.eventParticipant import EventParticipant
 
 @pytest.mark.integration
 def test_sendKioskDataKiosk():
     bnumber = "B00751864"
     eventid = 1
-    signin = sendkioskData(bnumber, eventid)
+    signin = sendUserData(bnumber, eventid)
     usersAttended = EventParticipant.select().where(EventParticipant.attended, EventParticipant.event == eventid)
     listOfAttended = [users.user.username for users in usersAttended]
 
     assert "neillz" in listOfAttended
+    assert "ramsayb2" not in listOfAttended
+
+@pytest.mark.integration
+def test_correctAlreadySignedIn():
+    bnumber = "B00751864"  # user neillz
+    eventid = 2
+    user, signedin = sendUserData(bnumber, eventid)
+    assert signedin
+    bnumber = "B00739736" # user ayisie
+    eventid = 1
+    user, signedin = sendUserData(bnumber, eventid)
+    assert not signedin
