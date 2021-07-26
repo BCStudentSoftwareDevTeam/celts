ab0e31b8d985b04efad6eaddd02b0a8dd55dee00
None <ayisie@ssdt2021ayisie.ssdt.berea.edu>
2021-07-26 16:56:29 -0400

    added new flashers at user mutiple required events

--- app/controllers/main/volunteerRegisterEvents.py
+++ app/controllers/main/volunteerRegisterEvents.py
@@ -1,5 +1,6 @@
 from app.models.event import Event
 from app.models.user import User
+from app.models.eventParticipant import EventParticipant
 from app.controllers.admin import admin_bp
 from app.controllers.events import events_bp
 from app.logic.userRsvpForEvent import userRsvpForEvent
@@ -13,22 +14,37 @@ def volunteerRegister():
     for the event they have clicked register for.
     """
     eventData = request.form
-    userId = User.get(User.username == 'khatts')
+    userId = User.get(User.username == g.current_user)
     eventId = eventData['eventId']
     isEligible, listOfRequirements = userRsvpForEvent(userId, eventId)
+    print(listOfRequirements)
     if not isEligible: # if they are banned
         flash(f"Cannot RSVP: {userId.firstName} is banned")
 
-    elif listOfRequirements is not None:
-        if not len(listOfRequirements) < 3:
+    elif len(listOfRequirements) > 0:
+        print(listOfRequirements)
+        if len(listOfRequirements) >= 3:
             reqListToString = ', '.join(listOfRequirements[:-1])
             reqListToString += ' or ' + listOfRequirements[-1]
-        elif not len(listOfRequirements) < 2:
+            flash(f"Warning: {userId.firstName} has not done the following required trainings: {reqListToString}")
+
+        elif len(listOfRequirements) == 2:
             reqListToString = ' or '.join(listOfRequirements)
+            flash(f"Warning: {userId.firstName} has not done the following required trainings: {reqListToString}")
+
         else:
             reqListToString = str(listOfRequirements[0])
-        flash(f"Warning: {userId.firstName} has not done the following required trainings: {reqListToString}")
+            reqListToString = 'Berea Buddies'
+            flash(f"Warning: {userId.firstName} has not done the following required trainings: {reqListToString}")
+
     else:
-        flash("Successfully registered for event!")
-        #FIXME: This is likely where you actually update the database
-    return redirect(url_for("events.showUpcomingEvent")) #FIXME: Have this redirect to the right page
+        # flash("Successfully registered for event!")
+        print("nice")
+        RSVPupdate =(EventParticipant.update({EventParticipant.rsvp: True})
+                         .where(EventParticipant.user == userId, EventParticipant.event == eventId))
+        print(RSVPupdate)
+        RSVPupdate.execute()
+        flash("Successfully registered for event!","success")
+        print('nice')
+    # return ''
+    return redirect(url_for("admin.addParticipants")) #FIXME: Have this redirect to the right page
