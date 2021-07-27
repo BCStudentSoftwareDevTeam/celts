6f733a17b2f64f330be504bb417f90c704f4979f
Zach Neill <neillz@ssdt2021neillz.ssdt.berea.edu>
2021-07-27 09:44:36 -0400

    test cleans up data after it runs

--- tests/code/test_signinKiosk.py
+++ tests/code/test_signinKiosk.py
@@ -5,15 +5,11 @@ from app.models.eventParticipant import EventParticipant
 @pytest.mark.integration
 def test_sendKioskDataKiosk():
     signin = sendUserData("B00751864", 1)
-    usersAttended = EventParticipant.select().where(EventParticipant.attended, EventParticipant.event == eventid)
+    usersAttended = EventParticipant.select().where(EventParticipant.attended, EventParticipant.event == 1)
     listOfAttended = [users.user.username for users in usersAttended]
 
     assert "neillz" in listOfAttended
     assert "ramsayb2" not in listOfAttended
 
-@pytest.mark.integration
-def test_correctAlreadySignedIn():
-    user, signedin = sendUserData("B00751864", 2)
-    assert signedin
-    user, signedin = sendUserData("B00739736", 1)
-    assert not signedin
+    (EventParticipant.update({EventParticipant.attended: False})
+                     .where(EventParticipant.user == "neillz", EventParticipant.event == 1)).execute()
