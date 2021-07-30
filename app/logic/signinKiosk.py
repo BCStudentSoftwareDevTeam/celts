3ede44c187636a76aaacde152661fe5a1c0d5a08
Tyler Parton <partont@ssdt2021partont.ssdt.berea.edu>
2021-07-30 11:31:35 -0400

    Resolved PR comments.

--- app/logic/signinKiosk.py
+++ app/logic/signinKiosk.py
@@ -1,7 +1,8 @@
-
+from app.models.event import Event
 from flask import Flask
 from app.models.eventParticipant import EventParticipant
 from app.models.user import User
+from app.logic.updateVolunteers import getEventLengthInHours
 from app.models.programBan import ProgramBan
 from app.logic.programEligibility import isEligibleForProgram
 
@@ -9,6 +10,7 @@ def sendUserData(bnumber, eventid, programid):
     """Accepts scan input and signs in the user. If user exists or is already
     signed in will return user and login status"""
     signInUser = User.get(User.bnumber == bnumber)
+    event = Event.get_by_id(eventid)
     if not isEligibleForProgram(programid, signInUser):
         userStatus = "banned"
     elif ((EventParticipant.select(EventParticipant.user)
@@ -21,9 +23,10 @@ def sendUserData(bnumber, eventid, programid):
             (EventParticipant.update({EventParticipant.attended: True})
                              .where(EventParticipant.user == signInUser, EventParticipant.event == eventid)).execute()
         else:
+            totalHours = getEventLengthInHours(event.timeStart, event.timeEnd,  event.startDate)
             EventParticipant.insert([{EventParticipant.user: signInUser,
                                       EventParticipant.event: eventid,
                                       EventParticipant.rsvp: False,
                                       EventParticipant.attended: True,
-                                      EventParticipant.hoursEarned: 0}]).execute()
-    return bNumberToUser, userStatus
+                                      EventParticipant.hoursEarned: totalHours}]).execute()
+    return signInUser, userStatus
