11dc915cd23b4179d54408bb99009b9fe13251c4
Gaston Jarju <jajug@berea.edu>
2021-08-06 09:30:02 -0400

    Resolved some PR commits with regards to the test_warning test suites.

--- tests/code/test_warning.py
+++ tests/code/test_warning.py
@@ -4,6 +4,13 @@ from app.logic.trackAttendees import trainedParticipants
 
 @pytest.mark.integration
 def test_prereqParticipants():
-    attendedPreq = prereqParticipant = trainedParticipants(1)
-
+    attendedPreq = trainedParticipants(1)
     assert "neillz" and "khatts" in attendedPreq
+
+    #test for program with no prereq
+    attendedPreq = trainedParticipants(4)
+    assert attendedPreq == []
+
+    #test for program that doesn't exist
+    attendedPreq = trainedParticipants(500)
+    assert attendedPreq == []
