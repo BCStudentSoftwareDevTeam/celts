fe1219d5ceb27188d9514499a2aa9d466e54e26f
Sandesh-L <lamichhanes2@berea.edu>
2021-08-31 18:16:35 -0400

    updated the tests so it would work with recent changes in the files

--- tests/code/test_proposals.py
+++ tests/code/test_proposals.py
@@ -11,7 +11,7 @@ def test_isCorrectData():
     courseDict = getProposalData('ramsayb2')
     assert 'Databases' in courseDict
     assert 'Brian Ramsay' in courseDict['Databases']['faculty']
-    assert 'Brian Ramsay, Zach Neill' in courseDict['Spanish Help']['faculty']
+    assert ['Brian Ramsay', 'Zach Neill'] == courseDict['Spanish Help']['faculty']
     assert 'Approved' in courseDict['Spanish Help']['status']
-    assert 'Spring A 2021' in courseDict['Spanish Help']['term']
+    assert 'Spring A 2021' in courseDict['Spanish Help']['term'].description
     assert not 'Internship' in courseDict
