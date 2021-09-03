e7b7d9af63bbe1c2b6b1e4594049223ae5cf626b
Zach Neill <neillz@ssdt2021neillz.ssdt.berea.edu>
2021-09-03 14:31:15 -0400

    altered email query to not backref every time

--- app/controllers/events/email.py
+++ app/controllers/events/email.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, flash, url_for, request, g
 from app.models.interest import Interest
+from app.models.user import User
 from app.models.eventParticipant import EventParticipant
 from app.controllers.events import events_bp
 
@@ -8,14 +9,19 @@ def emailVolunteers():
 
     emailInfo = request.form
     if emailInfo['emailRecipients'] == 'interested':    #email all students interested in the program
-        volunteersToEmail = Interest.select().where(Interest.program == emailInfo['programID'])
+        volunteersToEmail = User.select().join(Interest).where(Interest.program == emailInfo['programID'])
 
     elif emailInfo['emailRecipients'] == 'eventParticipant':  #email only people who rsvped
-        volunteersToEmail = EventParticipant.select().where(EventParticipant.event == emailInfo['eventID'])
+        volunteersToEmail = User.select().join(EventParticipant).where(EventParticipant.event == emailInfo['eventID'])
+
+    elif emailInfo['emailRecipients'] == 'yourself':  #email yourself; test purposes maybe
+        volunteersToEmail = User.select().where(User.username == g.current_user.username)
 
     else:
         print("ITS IMPRESSIVE HOW YOU MANAGED TO BREAK THIS")
-    print(f'Volunteers to email: {list(volunteersToEmail)}')
-    emails = [volunteer.user.email for volunteer in volunteersToEmail]
-    print(f'Whatever this is that Sandesh coded {emails}')
+        # I found a way :(
+        
+    emails = [volunteer.email for volunteer in volunteersToEmail]
+    print(f'Volunteers to email: {emails}')
+
     return redirect(url_for("events.events", term = 1))
