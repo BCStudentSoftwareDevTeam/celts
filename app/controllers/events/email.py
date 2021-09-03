22ff5fde60a34fdb5752466bb4125ce221d2af01
Sandesh-L <lamichhanes2@berea.edu>
2021-09-03 19:02:06 -0400

    trying to set up a emailHandler.

--- app/controllers/events/email.py
+++ app/controllers/events/email.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, flash, url_for, request, g
+from flask_mail import Message
 from app.models.interest import Interest
 from app.models.user import User
 from app.models.eventParticipant import EventParticipant
+from app.logic.emailHandler import *
 from app.controllers.events import events_bp
 
 @events_bp.route('/email', methods=['POST'])
@@ -20,8 +22,16 @@ def emailVolunteers():
     else:
         print("ITS IMPRESSIVE HOW YOU MANAGED TO BREAK THIS")
         # I found a way :(
-        
+
     emails = [volunteer.email for volunteer in volunteersToEmail]
     print(f'Volunteers to email: {emails}')
+
+    #testing the things in emailHandler file
+    default_cfg = load_config('app/config/default.yml') #this works yay
+    print(default_cfg['mail'])
+    mail = emailHandler(emailInfo)
+
+    #check mail_test.py in lsf to see what the following is trying to do
+    msg = Message(emailInfo['message'], recipients=['lamichhanes2@berea.edu'], html="<h3>Test</h3>Whooo",sender="support@bereacollege.onmicrosoft.com")
 
     return redirect(url_for("events.events", term = 1))
---
+++ app/logic/emailHandler.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, render_template
+import yaml, os
+from flask_mail import Mail, Message
+# from app.config
+# from app.config.production import *
+from app.models.emailTemplate import EmailTemplate
+from app import app
+import sys
+from pathlib import Path
+
+
+#borrowed from emailHandler file (and other places) in lsf
+
+def load_config(file):
+    """ This should be in a seperate file. prob in the config dir"""
+    with open(file, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    return cfg
+
+class emailHandler():
+    def __init__(self, emailInfo):
+        default = load_config('app/config/default.yml')
+        app.config.update(
+            MAIL_SERVER=default['mail']['server'],
+            MAIL_PORT=default['mail']['port'],
+            MAIL_USERNAME= default['mail']['username'],
+            MAIL_PASSWORD= default['mail']['password'],
+            REPLY_TO_ADDRESS= default['mail']['reply_to_address'],
+            MAIL_USE_TLS=default['mail']['tls'],
+            MAIL_USE_SSL=default['mail']['ssl'],
+            MAIL_DEFAULT_SENDER=default['mail']['default_sender'],
+            MAIL_OVERRIDE_ALL=default['mail']['override_addr'],
+            #ALWAYS_SEND_MAIL=default['ALWAYS_SEND_MAIL']
+        )
+
+        self.mail = Mail(app)
+
+    def send(self, message: Message):
+        if app.config['ENV'] == 'production' or app.config['ALWAYS_SEND_MAIL']:
+
+            # If we have set an override address
+            # if app.config['mail']['override_addr']:
+            #     message.html = "<b>Original message intended for {}.</b><br>".format(", ".join(message.recipients)) + message.html
+            #     message.recipients = [app.config['MAIL_OVERRIDE_ALL']]
+
+            message.reply_to = app.config["REPLY_TO_ADDRESS"]
+            self.mail.send(message)
+
+        elif app.config['ENV'] == 'testing':
+            # TODO: we really should have a way to check that we're sending emails that doesn't spam the logs
+            message.reply_to = app.config["REPLY_TO_ADDRESS"]
+            self.mail.send(message)
+            print("I did a thing in testing")##############################################################################################
+            pass
+        else:
+            print("ENV: {}. Email not sent to {}, subject '{}'.".format(app.config['ENV'], message.recipients, message.subject))
+
+        print("emails are being something'ed")#######################################################################################
