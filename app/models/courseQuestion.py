cc03e0d3b90fa9fdd55e12552bb3880bb68b2804
sreynitkhatt <khatts@berea.edu>
2021-09-30 10:20:04 -0400

    deleted file that was accidentally added

--- app/models/courseQuestion.py
+++
@@ -1,7 +0,0 @@
-from app.models import *
-from app.models.course import Course
-
-class CourseQuestion(baseModel):
-    course = ForeignKeyField(Course)
-    questionContent = CharField()
-    questionNumber = IntegerField()
