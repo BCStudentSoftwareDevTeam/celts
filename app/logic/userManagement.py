5d125fe8ff6ccd492db9f2c96f0d375dd136dac2
liberty Mupotsa <mupotsal@berea.edu>
2021-10-01 15:17:23 -0400

    added tests and the functions

--- app/logic/userManagement.py
+++ app/logic/userManagement.py
@@ -1,24 +1,25 @@
 from app.models.user import User
 
 def addCeltsAdmin(user):
-
     user = User.get_by_id(user)
     userSelect = User.get(User.username == user.username)
-    userSelect.isCeltsAdmin = 1 
+    userSelect.isCeltsAdmin = True
     userSelect.save()
 
-
 def addCeltsStudentStaff(user):
-
-    return 0
-
-def removeCeltsAdmin(user):
-
     user = User.get_by_id(user)
     userSelect = User.get(User.username == user.username)
-    userSelect.isCeltsAdmin = 0
+    userSelect.isCeltsStudentStaff = True
+    userSelect.save()
+
+def removeCeltsAdmin(user):
+    user = User.get_by_id(user)
+    userSelect = User.get(User.username == user.username)
+    userSelect.isCeltsAdmin = False
     userSelect.save()
 
 def removeCeltsStudentStaff(user):
-
-    return 0
+    user = User.get_by_id(user)
+    userSelect = User.get(User.username == user.username)
+    userSelect.isCeltsStudentStaff = False
+    userSelect.save()
--- tests/code/test_userManagement.py
+++ tests/code/test_userManagement.py
@@ -1,17 +1,27 @@
 import pytest
-from app.logic.userManagement import addCeltsAdmin, removeCeltsAdmin
+from app.logic.userManagement import addCeltsAdmin, removeCeltsAdmin,addCeltsStudentStaff, removeCeltsStudentStaff
 from app.models.user import User
 
 @pytest.mark.integration
 def test_addCeltsAdmin():
-
     user = "agliullovak"
     current_user = User.get(User.username == user)
     assert current_user.isCeltsAdmin == False
     addCeltsAdmin(current_user)
     current_user = User.get(User.username == user)
     assert current_user.isCeltsAdmin == True
-
     removeCeltsAdmin(current_user)
     current_user = User.get(User.username == user)
     assert current_user.isCeltsAdmin == False
+
+def test_addCeltsStudentStaff():
+    user = "mupotsal"
+    current_user = User.get(User.username == user)
+    assert current_user.isCeltsAdmin == False
+    addCeltsStudentStaff(current_user)
+    current_user = User.get(User.username == user)
+    assert current_user.isCeltsAdmin == True
+    removeCeltsStudentStaff(current_user)
+    current_user = User.get(User.username == user)
+    assert current_user.isCeltsAdmin == False
+    
