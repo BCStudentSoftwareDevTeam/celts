e2be7246bd144b7dc1c6d2f6362d9359dfd6a465
Zach Neill <neillz@ssdt2021neillz.ssdt.berea.edu>
2021-10-12 17:02:12 -0400

    moving html onclick to js equivalent

--- app/static/js/userManagement.js
+++ app/static/js/userManagement.js
@@ -45,33 +45,10 @@ $(document).ready(function() {
   $("#removeCeltsStudentStaff").on("click", function() {
     submitRemoveCeltsStudentStaff("removeCeltsStudentStaff")
   });
-
-  function selectTerm(totalTerms, term){
-    console.log("yo")
-    $("#termInput").val(term);
-    for (i=1; i<=totalTerms; i++){
-      if ($('#termFormID_' + i).hasClass('active')){
-       $('#termFormID_' + i).removeClass('active');
-      }
-    }
-    $('#termFormID_' + term).addClass('active');
-  };
-
-  function submitTerm(){
-    termInfo = {id: $("#termInput").val()}
-    $.ajax({
-      url: "/changeCurrentTerm",
-      type: "POST",
-      data: termInfo,
-      success: function(s){
-          location.reload()
-      },
-      error: function(error, status){
-          console.log(error, status)
-      }
-    })
-  };
-
+  for
+    $("#removeCeltsStudentStaff").on("click", function() {
+      submitRemoveCeltsStudentStaff("removeCeltsStudentStaff")
+    });
 });
 
 function submitAdmin(method){
@@ -157,3 +134,28 @@ function submitRemoveCeltsStudentStaff(method){
 
   })
 }
+
+function clickTerm(totalTerms, term){
+  $("#termInput").val(term);
+  for (i=1; i<=totalTerms; i++){
+    if ($('#termFormID_' + i).hasClass('active')){
+     $('#termFormID_' + i).removeClass('active');
+    }
+  }
+  $('#termFormID_' + term).addClass('active');
+};
+
+function submitTerm(){
+  termInfo = {id: $("#termInput").val()}
+  $.ajax({
+    url: "/changeCurrentTerm",
+    type: "POST",
+    data: termInfo,
+    success: function(s){
+        location.reload()
+    },
+    error: function(error, status){
+        console.log(error, status)
+    }
+  })
+};
