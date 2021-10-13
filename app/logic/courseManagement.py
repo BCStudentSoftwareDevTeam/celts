93e9ae86a91a164e127e9bf7097a7b6cc84e0a5c
Tyler Parton <tylerpar99@gmail.com>
2021-10-13 14:56:17 -0400

    Worked on handling of multiple instructors. In progress.

--- app/logic/courseManagement.py
+++ app/logic/courseManagement.py
@@ -7,7 +7,7 @@ from app.models.term import Term
 def pendingCourses(termId):
     '''
     Queries the database to get all the neccessary information for
-    pending courses.
+    non approved and non completed courses.
     '''
 
     pendingCourses = (CourseInstructor.select(Course.courseName, Term.description, CourseInstructor.user, Course.status)
@@ -17,21 +17,34 @@ def pendingCourses(termId):
                     .join(Term)).where(CourseInstructor.course.term.id == termId, Course.status.status != "Approved", Course.status.status != "Completed")
 
 
+
     return pendingCourses
+
+
 def approvedCourses(termId):
     '''
     Queries the database to get all the neccessary information for
-    pending courses.
+    approved courses.
     '''
 
-    approvedCourses = (CourseInstructor.select(Course.courseName, Term.description, CourseInstructor.user, Course.status)
+    approvedCourses = (CourseInstructor.select(Course.courseName, Course.id, Term.description, CourseInstructor.user, Course.status)
                     .join(Course)
                     .join(CourseStatus)
                     .switch(Course)
                     .join(Term)).where(CourseInstructor.course.term.id == termId, Course.status.status == "Approved")
 
-    for a in approvedCourses:
-        print(a.user)
+    courseIds = []
+    for course in approvedCourses:
+        courseIds.append(course.course.id)
 
-
+    approvedCourseInstructor = getinstructorData(courseIds)
     return approvedCourses
+
+def getinstructorData(courseIds):
+    """
+    Gets and instructor object for the course id's given.
+    """
+
+    instructorDict = {}
+    for i in SLCourseInstructor:
+        instructorDict.setdefault(i.courseId, []).append(i.user.firstName + " " + i.user.lastName)
