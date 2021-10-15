2080ca474799b661ebd7b7ca1fd666f213d45273
Zach Neill <neillz@ssdt2021neillz.ssdt.berea.edu>
2021-10-15 15:10:09 -0400

    added tests for term logic

--- tests/code/test_userManagement.py
+++ tests/code/test_userManagement.py
@@ -1,8 +1,8 @@
 import pytest
-from app.logic.userManagement import addCeltsAdmin, removeCeltsAdmin,addCeltsStudentStaff, removeCeltsStudentStaff
+from app.logic.userManagement import addCeltsAdmin, removeCeltsAdmin,addCeltsStudentStaff, removeCeltsStudentStaff, changeCurrentTerm
 from app.models.user import User
 from peewee import DoesNotExist
-
+from flask import g
 @pytest.mark.integration
 def test_modifyCeltsAdmin():
     user = "agliullovak"
@@ -35,3 +35,17 @@ def test_modifyCeltsStudentStaff():
         addCeltsStudentStaff("asdf")
     with pytest.raises(DoesNotExist):
         removeCeltsStudentStaff("1234")
+
+def test_changeCurrentTerm():
+    # test via g.current_term
+    changeCurrentTerm(2)
+    assert g.current_term == 2
+    # test via isCurrentTerm
+    newTerm = changeCurrentTerm(1)
+    assert newTerm.isCurrentTerm
+
+def test_invalidTermInputs():
+    with pytest.raises(DoesNotExist):
+        changeCurrentTerm(100)
+    with pytest.raises(DoesNotExist):
+        changeCurrentTerm("womp")
