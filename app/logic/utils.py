08ab93610680372ea4f68dce89ac019341566473
Brian Ramsay <ramsayb2@berea.edu>
2021-10-26 11:42:44 -0400

    Add docstring for selectSurroundingTerms

--- app/logic/utils.py
+++ app/logic/utils.py
@@ -2,9 +2,17 @@ import collections
 from app.models.term import Term
 
 def selectSurroundingTerms(currentTerm, prevTerms=2):
+    """
+    Returns a list of term objects around the provided Term object for the current term. 
+    Chooses the previous terms according to the prevTerms parameter (defaulting to 2), 
+    and then chooses terms for the next two years after the current term. 
+
+    To get only the current and future terms, pass prevTerms=0.
+    """
     startTerm = max(1, currentTerm.id - prevTerms)
-    surroundingTerms = (Term.select().where(Term.id >= startTerm)
-                                .where((Term.year <= currentTerm.year + 2)))
+    surroundingTerms = (Term.select()
+                            .where(Term.id >= startTerm)
+                            .where((Term.year <= currentTerm.year + 2)))
 
     return surroundingTerms
 
