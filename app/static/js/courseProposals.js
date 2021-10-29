a64d6444de850bdd18c756655fe486c7c9b0c65d
Zach Neill <neillz@ssdt2021neillz.ssdt.berea.edu>
2021-10-29 16:40:14 -0400

    merged development and improved js comments

--- app/static/js/courseProposals.js
+++ app/static/js/courseProposals.js
@@ -1,8 +1,6 @@
 
-// withdrawing a course
-
 $(document).ready(function() {
-  //if they decide not to withdraw, change selection back to "select action"
+  // if they decide not to withdraw, change selection back to "select action"
   $('#withdrawModal').on('hidden.bs.modal', function () {
     $('.form-select').val('---');
   });
@@ -12,23 +10,22 @@ $(document).ready(function() {
 });
 
 function changeAction(action){
+  // decides what to do based on selection
   if (action.value=="Renew"){
     // Renew
-
   } else if (action.value=="View"){
     // View
-
   } else if (action.value=="Withdraw"){
     courseID = action.id;
     $('#courseToRemove').val(courseID);
     $('#withdrawModal').modal('show');
-
   } else if(action.value=="Edit"){
     // Edit
   }
 }
 
 function withdraw(){
+  // uses hidden label to withdraw course
   courseID = $("#courseToRemove").val()
   $.ajax({
     url: "/courseProposals/"+courseID+"/withdraw/",
