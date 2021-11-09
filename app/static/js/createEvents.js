b40e510725c3be2495f983eac167052b213206a8
sreynitkhatt <khatts@berea.edu>
2021-11-09 10:37:18 -0500

    added document.ready to js

--- app/static/js/createEvents.js
+++ app/static/js/createEvents.js
@@ -1,5 +1,5 @@
 
-$(function(){
+$(document).ready(function(){
   //Browser detection was implemented using a solution found  here https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
   var isFirefox = typeof InstallTrigger !== 'undefined';
   var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
