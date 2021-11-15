30b263ec7b595f8cf0f772b93e786b88e558679e
Ebenezer <ayisie@berea.edu>
2021-11-15 10:47:46 -0500

    filter search bar

--- app/static/js/manageServiceLearningFaculty.js
+++ app/static/js/manageServiceLearningFaculty.js
@@ -1,8 +1,32 @@
 
+$(document).ready(function() {
+  $('[data-bs-toggle="tooltip"]').tooltip();
+// Search functionalities from the volunteer table in the UI
+  $("#{{inputId}}").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#facultyTable").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
 
+$(document).ready(function()  {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
 
-$('#table table-striped').DataTable( {
-    buttons: [
-        { extend: 'remove', editor: myEditor }
-    ]
-} );
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+});
