892b91073b6afbe00074f0bcb82075bde4a5e3c4
sreynitkhatt <khatts@berea.edu>
2021-11-16 12:21:47 -0500

    rearranged and added new stuff to backgroundCheck table

--- app/models/backgroundCheck.py
+++ app/models/backgroundCheck.py
@@ -1,6 +1,11 @@
 from app.models import *
 from app.models.user import User
+from app.models.backgroundCheckType import BackgroundCheckType
 
 class BackgroundCheck(baseModel):
     user = ForeignKeyField(User)
+    type = ForeignKeyField(BackgroundCheckType)
     passBackgroundCheck = BooleanField(default=False)
+
+    class Meta:
+        primary_key=CompositeKey('user', 'type')
