8cf8dd05aecd6235d7c3ffe7d9181379e20cc26f
Ebenezer <ayisie@berea.edu>
2021-11-19 11:51:11 -0500

    changed table to datatables and fix filter search

--- app/static/js/manageServiceLearningFaculty.js
+++ app/static/js/manageServiceLearningFaculty.js
@@ -1,11 +1,10 @@
+$(document).ready( function () {
+   var table =  $('#myTable').DataTable({
+   "fnDrawCallback": function(oSettings) {
+     if ($('#myTable tr').length < 11) {
+         $('.dataTables_length').hide();
+       }
 
-$(document).ready(function() {
-  $('[data-bs-toggle="tooltip"]').tooltip();
-// Search functionalities from the volunteer table in the UI
-  $("#{{inputId}}").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#facultyTable").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  })
+    }
+  });
 });
