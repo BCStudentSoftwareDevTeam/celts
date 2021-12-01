78654444ce5e38e2586acf1717bc2e2bacaf7837
hmanalai <hila_manalai@berea.edu>
2021-12-01 15:53:32 -0500

    branstorming ideas for how to structure email handler

--- app/logic/emailHandler.py
+++ app/logic/emailHandler.py
@@ -11,6 +11,53 @@ import webbrowser
 import time
 from pathlib import Path
 
+"""
+The Requirements:
+1. Send Email - Components:- Sender, Receiver, Email Body
+2. Edit Email - Management Page
+3. Create recipients groups? - Would they ever create manual groups for anything?
+   - If so, where would we save this? A new table? Sounds awful.
+
+TODO:
+
+- A general method that sets up the connection (would this need to be called when email is sent?)
+
+- Method to receive all the info needed: Sender, Receiver, email_topic, etc.
+  - This method will also clean up the data so it can be used in other methods.
+
+- Method to retrieve email_template
+  - What should be the parameter for retrieval? Not the subject. We should find a better identifies.
+
+- Method to update email_template (after admin edits the tempate this method could be called).
+  - We could check which field(s) needs update in this method. Or create a new one.
+
+- Method to send the email.
+
+- Methods to get the email addresses of all categories (To), such as interested/participating etc.
+  - How many categories/groups will be there?
+
+ - Method to replace_fields in email_template
+   - things like recipient name, event name, description, date and time
+   - Come up with a smart way to do this.
+   - This can be called inside send_email method, or another method called before that.
+     because at that point we should have all the fields info
+
+Notes:
+- All the backend logic/processing should happen in the Email Handler class.
+  For instance, the code in /events/email.py can be all moved to the class.
+  So that in email.py, we only do one or two function calls.
+
+- Email Handler class shouldn't be dependent on form data.
+  It should have a method that receives data (could be in any one format),
+  cleans it up. Then other methods will use it.
+
+- Why is the updateSenderEmail updating config? Instead of updating config,
+  the class can have variables that hold sender, receiver, etc. info.
+  first it will fetch from the config. If need updated, then the variable alone would be updated.
+"""
+
+
+
 def getInterestedEmails(programID = None):
     """ Gets emails of students interested in a program. """
     volunteersToEmail = User.select().join(Interest).where(Interest.program == programID)
