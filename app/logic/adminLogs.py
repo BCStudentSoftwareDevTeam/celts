2c65b22f731dcfe28c0a5e65df0458a613f3a1c6
liberty Mupotsa <mupotsal@berea.edu>
2021-12-17 13:44:27 -0500

    made changes to logs

--- app/logic/adminLogs.py
+++ app/logic/adminLogs.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from app.models.user import User
 from app.models.adminLogs import AdminLogs
 
-
 def createLog(content):
     date = datetime.strptime(datetime.now().strftime('%Y %m %d %H:%M:%S'),'%Y %m %d %H:%M:%S')
     entry = AdminLogs.create(createdBy=g.current_user,createdOn=date,logContent=content)
