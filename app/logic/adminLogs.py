e5ebf3dd636f585646a51ae3a04f4ae883b534a8
liberty Mupotsa <mupotsal@berea.edu>
2022-02-23 11:29:10 -0500

    creating tests

--- app/logic/adminLogs.py
+++ app/logic/adminLogs.py
@@ -4,6 +4,6 @@ from app.models.user import User
 from app.models.adminLogs import AdminLogs
 
 def createLog(content):
-    date = datetime.strptime(datetime.now().strftime('%Y %m %d %H:%M:%S'),'%Y %m %d %H:%M:%S')
+    date = datetime.now()
     entry = AdminLogs.create(createdBy=g.current_user,createdOn=date,logContent=content)
     entry.save()
---
+++ tests/code/test_logsCreation.py
@@ -0,0 +1,13 @@
+import pytest
+from flask import g
+from app.models.adminLogs import AdminLogs
+from app.logic.adminLogs import createLog
+from app.models import mainDB
+
+@pytest.mark.integration
+def test_createLogs():
+    with mainDB.atomic() as transaction:
+        currentLogsCount = len(list(AdminLogs.select()))
+        createLog("This is a test log")
+        assert len(list(AdminLogs.select())) == currentLogsCount +1
+        transaction.rollback()
