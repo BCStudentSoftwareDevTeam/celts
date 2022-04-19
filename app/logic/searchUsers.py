70c9f16e82537d287324d45b96183f85870761f6
Tyler Parton <tylerpar99@gmail.com>
2022-04-19 14:25:11 -0400

    this pr is tested and fixes search logic clearing. Can now be toggled.

--- app/logic/searchUsers.py
+++ app/logic/searchUsers.py
@@ -3,6 +3,7 @@ from app.models.user import User
 def searchUsers(query):
     '''Accepts user input and queries the database returning results that matches user search'''
     query = query.strip()
+    print(query)
     search = query.upper()
     splitSearch = search.split()
     resultsDict = {}
@@ -12,6 +13,8 @@ def searchUsers(query):
 
     if len(splitSearch) == 1: #search for first or last name
         results = User.select().where(User.isStudent & (User.firstName ** firstName | User.lastName ** firstName))
+        for r in results:
+            print(r)
         for participant in results:
             if participant not in resultsDict:
                 resultsDict[participant.username]=model_to_dict(participant)
