a5c793ca283869b233def6652d2ca5044cb0fa83
Tyler Parton <tylerpar99@gmail.com>
2022-04-22 13:54:54 -0400

    made sure routes were not accessible as a celtstudentstaff.

--- app/controllers/main/routes.py
+++ app/controllers/main/routes.py
@@ -194,5 +194,8 @@ def getAllCourseIntructors():
     """
     This function selects all the Intructors Name and the previous courses
     """
-    courseDict = getCourseDict()
-    return render_template('/main/manageServiceLearningFaculty.html', courseInstructors = courseDict)
+    if g.current_user.isCeltsAdmin:
+        courseDict = getCourseDict()
+        return render_template('/main/manageServiceLearningFaculty.html', courseInstructors = courseDict)
+    else:
+        abort(403)
