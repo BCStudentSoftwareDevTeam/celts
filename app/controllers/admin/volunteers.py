abf4e2ce3dc5bf9360cbba69dfa119ec7142e847
madinabonu <solijonovam@berea.edu>
2022-06-23 13:49:45 -0400

    deletes 'isPastEvent' from the controller

--- app/controllers/admin/volunteers.py
+++ app/controllers/admin/volunteers.py
@@ -50,7 +50,6 @@ def trackVolunteersPage(eventID):
         event.timeEnd,
         event.startDate)
 
-    isPastEvent = (datetime.now() >= datetime.combine(event.startDate, event.timeStart))
 
     return render_template("/events/trackVolunteers.html",
         eventRsvpData=list(eventRsvpData),
@@ -58,7 +57,6 @@ def trackVolunteersPage(eventID):
         eventLength=eventLengthInHours,
         program=program,
         event=event,
-        isPastEvent=isPastEvent,
         trainedParticipantsList=trainedParticipantsList)
 
 @admin_bp.route('/eventsList/<eventID>/track_volunteers', methods=['POST'])
