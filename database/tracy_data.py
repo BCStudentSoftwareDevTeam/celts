a1f027d20dcb573743638de678d38f6f478da39c
Garrett Clark <clarkg@berea.edu>
2022-06-24 11:15:58 -0400

    Created updateRecords function that runs every 24 hours. Also changed the user model to allow phone numbers to be null

--- database/tracy_data.py
+++ database/tracy_data.py
@@ -1,27 +1,50 @@
-import pyodbc # module that accesses ODBC (MySQL) databases
+import pyodbc
 from app.models.user import User
+import schedule
+import time
 
-
-details = {                     # information to access server
-    "user": "ute_limited",
-    "password": "VB6PlU$WcbDBqZ3m0IDX",
-    "server": "timemachine1sql.berea.edu",
-    "db": "UTE"
-}
 def main():
-    # uniform resource identifier contains name which refers to an object in the web
+    """
+    This function runs the updateRecords function once every 24 hours.
+    """
+    schedule.every(24).hours.do(updateRecords)
+
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
+
+def updateRecords():
+    """
+    Sets up a connection with an external database and creates new entries for both
+    students and faculty in the Users table of the Celts database.
+    Currently pulling from Tracy, might need to be update in the future.
+    """
+    details = {
+        "user": "ute_limited",
+        "password": "VB6PlU$WcbDBqZ3m0IDX",
+        "server": "timemachine1sql.berea.edu",
+        "db": "UTE"
+    }
+    getFacultyUserData(details) # TODO: make it so function updates entries based on bnumber
+    getStudentUserData(details) # TODO: make it so function updates entries based on bnumber
+
+def getFacultyUserData(details):
+    """
+    This function pulls all the faculty data from another database and creates new users
+    in the Celts user table.
+    Currently pulling data from the Tracy database.
+    """
     pyodbc_uri = 'DRIVER=FreeTDS;SERVER={};PORT=1433;DATABASE={};UID={};PWD={};TDS_Version=8.0;'.format(details['server'],details['db'],details['user'],details['password'])
 
     pyconn = pyodbc.connect(pyodbc_uri)  # connects a tcp based client socket to a tcp based server socket
-    c = pyconn.cursor()  # allows python to execute sql database command??
-    fub = 0
-    for row in c.execute('select * from STUSTAFF'):
+    c = pyconn.cursor()  # allows python to execute sql database commands
 
+    for row in c.execute('select * from STUSTAFF'):
         try:
-            user = {"username": getUsernameFromEmail(row[4]),
+            user = {"username": getUsernameFromEmail(row[4]), #Tracy does not have this information
                     "bnumber": row[1],
                     "email": row[4],
-                    "phoneNumber": "000000",
+                    "phoneNumber": None, # None currently as Tracy does not have phone #'s in database
                     "firstName": row[2],
                     "lastName": row[3],
                     "isStudent": False,
@@ -29,22 +52,53 @@ def main():
                     "isCeltsAdmin":False,
                     "isCeltsStudentStaff": False
             }
-            fub += 1
+            User.insert(user).execute()
+        except Exception as e:
+            # Duplicate entry exceptions are expected due to user data already being
+            # in the User table of the Celts database
+            print(e, " Duplicate entry exceptions are expected.")
 
-            #User.insert(user).execute()
-        except:
-            pass
+def getStudentUserData(details):
+    """
+    This function pulls all the student data from another database and creates new users
+    in the Celts user table.
+    Currently pulling data from the Tracy database.
+    """
+    pyodbc_uri = 'DRIVER=FreeTDS;SERVER={};PORT=1433;DATABASE={};UID={};PWD={};TDS_Version=8.0;'.format(details['server'],details['db'],details['user'],details['password'])
+    pyconn = pyodbc.connect(pyodbc_uri)
+    c = pyconn.cursor()
+
+    for row in c.execute('select * from STUDATA'):
+        try:
+            user = {"username": getUsernameFromEmail(row[9]),
+                    "bnumber": row[1],
+                    "email": row[9],
+                    "phoneNumber": None,
+                    "firstName": row[2],
+                    "lastName": row[3],
+                    "isStudent": True,
+                    "isFaculty": False,
+                    "isCeltsAdmin":False,
+                    "isCeltsStudentStaff": False
+            }
+            User.insert(user).execute()
+        except Exception as e:
+            # Duplicate entry exceptions are expected due to user data already being
+            # in the User table of the Celts database
+            print(e, " Duplicate entry exceptions are expected.")
 
 def getUsernameFromEmail(emailStr):
-    newEmailStr = ''
-    
-    if "@berea.edu" not in emailStr:
-        fub += 1
-        print(fub,") ", emailStr)
+    """
+    This function loops through a users email until the index is an "@" and creates the
+    username.
+    This function will most likely not be needed once we access data from another DB.
+    """
+    username = ''
     for i in emailStr:
         if i != '@':
-            newEmailStr += i
+            username += i
         else:
-            return newEmailStr
+            return username
 
-main()
+if __name__ == '__main__':
+    main()
