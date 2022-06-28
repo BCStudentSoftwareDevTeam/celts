ee347c30c2f55234915b9722297e6b8acbce2d27
Andersonstettner <andersonwcstettner@gmail.com>
2022-06-28 12:05:08 -0400

    added the rollbaack back to where it was originally and kept it in the new spot

--- tests/code/test_emailHandler.py
+++ tests/code/test_emailHandler.py
@@ -170,7 +170,8 @@ def test_recipients_category():
             email.process_data()
             assert email.recipients == [User.get_by_id("partont"), User.get_by_id("khatts")]
             newTrainedStudent.delete_instance()
-
+            
+            transaction.rollback()
 
             # Test a program that should have nothing in banned users and nothing in All Volunteer:
             raw_form_data = {"templateIdentifier": "Test",
