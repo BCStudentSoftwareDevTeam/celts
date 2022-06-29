9aa959c45f0023e684555e88acf65f7e873b0f32
Andersonstettner <andersonwcstettner@gmail.com>
2022-06-29 10:05:51 -0400

    adding conditional to check when allVolunteer was completed

--- app/logic/emailHandler.py
+++ app/logic/emailHandler.py
@@ -102,7 +102,13 @@ class EmailHandler:
                 .where(EventRsvp.event==self.event.id))
 
         if recipients_category == "Eligible Students":
-            term = Term.select()
+            term = list(Term.select().order_by(Term.id))
+            prevTerm = term[-1]
+            prevSemester, prevYear = prevTerm.description.split()
+
+            if prevSemester == "Summer":
+
+
             bannedUsers = ProgramBan.select(ProgramBan.user_id).where((ProgramBan.endDate > datetime.now()) | (ProgramBan.endDate is None), ProgramBan.program_id.in_([p.id for p in self.program_ids]))
             allVolunteer = Event.select().where(Event.isAllVolunteerTraining == True,)
             recipients = User.select().join(EventParticipant).where(User.username.not_in(bannedUsers), EventParticipant.event.in_(allVolunteer))
