96bfa919d8f9c176b2f632f1e5cb6493fd2a0cf1
bledsoef <bledsoef@berea.edu>
2022-07-01 10:20:29 -0400

    Added minor changes, briefly putting on pause until another pull request is merged.

--- app/scripts/sendEventReminderEmails.py
+++ app/scripts/sendEventReminderEmails.py
@@ -1,14 +1,21 @@
 from app.logic.emailHandler import EmailHandler
 from app.models.event import Event
+from app.models.term import Term
+from app.models.programEvent import ProgramEvent
+from app.models.program import Program
 from datetime import date, datetime, timedelta
 
 def checkForEvents():
     currentDate = date.today().strftime("%m/%d/%Y")
     today = datetime.strptime(currentDate,"%m/%d/%Y")
     tomorrowDate = today + timedelta(days=1)
+    currentTerm = Term.get(Term.id).where(Term.isCurrentTerm==1)
     events = list(Event.select().where(Event.startDate==tomorrowDate))
     for i in range(len(events)):
-        createData(events[i].id)
+        programId = ProgramEvent.get(ProgramEvent.program).where(Program.event==events[i].id)
+        senderName = Program.get(Program.senderName)
+        createData(events[i].id, programId, currentTerm, "Reminder", "Interested", )
 
-def createData(eventId):
+
+def createData(eventId, programId, selectedTerm, templateIdentifier, recipientsCategory, emailSender, subject, body):
     event = Event.get_by_id(eventId)
