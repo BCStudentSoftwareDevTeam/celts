96c2e87ad3098ffde2fb6eba8d81bf7cd358fbb7
thawpt <thawp@berea.edu>
2022-07-05 13:15:46 -0400

    worked on the checkForEvents function

--- app/scripts/sendEventReminderEmails.py
+++ app/scripts/sendEventReminderEmails.py
@@ -5,30 +5,33 @@ from app.models.programEvent import ProgramEvent
 from app.models.program import Program
 from app.models.user import User
 from app.models.emailTemplate import EmailTemplate
+from peewee import DoesNotExist
 from datetime import date, datetime, timedelta
 
 def checkForEvents():
-    tomorrowDate = date.today() + timedelta(days=1)
-    currentTerm = Term.select(Term.id).where(Term.isCurrentTerm==1)
-    events = list(Event.select().where(Event.startDate==tomorrowDate))
+    try:
+        tomorrowDate = date.today() + timedelta(days=1)
+        currentTerm = Term.select(Term.id).where(Term.isCurrentTerm==1)
+        events = list(Event.select().where(Event.startDate==tomorrowDate))
+        template = EmailTemplate.get(purpose = "Reminder")
+        templateSubject = template.subject
+        templateBody = template.body
+        for event in events:
+            programId = event.singleProgram
+            senderName = programId.emailSenderName
+            emailData = {"eventID":event.id,
+                            "programID":programId,
+                            "term":currentTerm,
+                            "templateIdentifier":"Reminder",
+                            "recipientsCategory":"Interested",
+                            "senderName":senderName,
+                            "subject":templateSubject,
+                            "body":templateBody}
+            sendEmail = EmailHandler(emailData, "172.31.3.239:8080", User.get_by_id("ramsayb2"))
+            sendEmail.send_email()
 
-    template = EmailTemplate.get(purpose = "Reminder")
-    templateSubject = template.subject
-    templateBody = template.body
-    for event in events:
-        programId = event.singleProgram
-        senderName = Program.get(Program.emailSenderName)
-        emailData = {"EventId":event.id,
-                        "ProgramId":programId,
-                        "term":currentTerm,
-                        "purpose":"Reminder",
-                        "recipientsCategory":"Interested",
-                        "senderName":senderName,
-                        "subject":templateSubject,
-                        "body":templateBody}
-        sendEmail = EmailHandler(emailData, "172.31.2.2:8080", User.get_by_id("ramsayb2"))
-        sendEmail.send_email()
-
+    except (DoesNotExist, IndexError) as e:
+        print(e)
 def main():
     checkForEvents()
 
