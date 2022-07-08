f5fdb7825ec76d81cf81ffc67a1df74dffb2c95f
Andersonstettner <andersonwcstettner@gmail.com>
2022-07-08 15:03:31 -0400

    merge conflicts

--- tests/code/test_event_list.py
+++ tests/code/test_event_list.py
@@ -6,43 +6,43 @@ from app.models.event import Event
 from app.models.event import Term
 from app.logic.events import getStudentLedEvents,  getTrainingEvents, getBonnerEvents, getNonProgramEvents
 
-@pytest.mark.integration
-@pytest.fixture
-def training_events():
-    # testEvent = Event.create(name = "Test Student Lead",
-    #                         term = 3,
-    #                         description = "event for testing",
-    #                         timeStart = "18:00:00",
-    #                         timeEnd = "21:00:00",
-    #                         location = "basement",
-    #                         startDate = 2021-12-12,
-    #                         endDate = 2021-12-13)
-
-    testProgramEvent = ProgramEvent.create(program = 5, event = 1)
-    return testProgramEvent
-    # yield testProgramEvent
-    #
-    #testEvent.delete_instance(testProgramEvent)
-
-@pytest.mark.integration
-def test_studentled_events(training_events):
-
-
-
-@pytest.mark.integration
-def test_training_events(training_events):
-    testProgramEvent = getTrainingEvents(3)
-
-    assert testProgramEvent
-
-@pytest.mark.integration
-def test_bonner_events(training_events):
-    testProgramEvent = getBonnerEvents(3)
-
-    assert testProgramEvent
-
-@pytest.mark.integration
-def test_nonProgram_events(training_events):
-    testProgramEvent = getNonProgramEvents(6)
-
-    assert testProgramEvent
+# @pytest.mark.integration
+# @pytest.fixture
+# def training_events():
+#     # testEvent = Event.create(name = "Test Student Lead",
+#     #                         term = 3,
+#     #                         description = "event for testing",
+#     #                         timeStart = "18:00:00",
+#     #                         timeEnd = "21:00:00",
+#     #                         location = "basement",
+#     #                         startDate = 2021-12-12,
+#     #                         endDate = 2021-12-13)
+#
+#     testProgramEvent = ProgramEvent.create(program = 5, event = 1)
+#     return testProgramEvent
+#     # yield testProgramEvent
+#     #
+#     #testEvent.delete_instance(testProgramEvent)
+#
+# @pytest.mark.integration
+# def test_studentled_events(training_events):
+#
+#
+#
+# @pytest.mark.integration
+# def test_training_events(training_events):
+#     testProgramEvent = getTrainingEvents(3)
+#
+#     assert testProgramEvent
+#
+# @pytest.mark.integration
+# def test_bonner_events(training_events):
+#     testProgramEvent = getBonnerEvents(3)
+#
+#     assert testProgramEvent
+#
+# @pytest.mark.integration
+# def test_nonProgram_events(training_events):
+#     testProgramEvent = getNonProgramEvents(6)
+#
+#     assert testProgramEvent
