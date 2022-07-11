e61b9681c5165b87c90b237551c94c02d3644def
qasema <qasema@berea.edu>
2022-07-11 09:47:22 -0400

    remove the import of previous function

--- app/logic/volunteers.py
+++ app/logic/volunteers.py
@@ -9,7 +9,7 @@ from app.models.programManager import ProgramManager
 from datetime import datetime, date
 from app.logic.adminLogs import createLog
 from app.logic.events import getEvents
-from app.logic.userManagement import hasPrivilege
+
 
 
 
