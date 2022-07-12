ea43c2fdbb249c9dd45f2e776101bcfe4156ddf2
Andersonstettner <andersonwcstettner@gmail.com>
2022-07-12 12:25:24 -0400

    Completed all Event List tests

--- tests/code/test_event_list.py
+++ tests/code/test_event_list.py
@@ -18,13 +18,46 @@ def training_events():
                             timeStart = "18:00:00",
                             timeEnd = "21:00:00",
                             location = "basement",
+                            isTraining = True,
                             startDate = 2021-12-12,
                             endDate = 2021-12-13)
 
     testProgramEvent = ProgramEvent.create(program = 2 , event = testEvent)
-    yield testProgramEvent
 
+    yield testProgramEvent
     testEvent.delete_instance(testProgramEvent)
+
+@pytest.mark.integration
+@pytest.fixture
+def special_bonner():
+    bonnerEvent = Event.create(name = "Test For Bonner",
+                            term = 2,
+                            description = "Special event test for Bonner",
+                            timeStart = "19:00:00",
+                            timeEnd = "22:00:00",
+                            location = "moon",
+                            startDate = 2021-12-12,
+                            endDate = 2021-12-13)
+
+    specialForBonner = ProgramEvent.create(program = 5, event = bonnerEvent)
+
+    yield specialForBonner
+    bonnerEvent.delete_instance(specialForBonner)
+
+@pytest.mark.integration
+@pytest.fixture
+def special_nonProgram():
+        nonProgramEvent = Event.create(name = "Test for nonProgram",
+                                term = 2,
+                                description = "Special event test for nonProgram",
+                                timeStart = "19:00:00",
+                                timeEnd = "22:00:00",
+                                location = "moon",
+                                startDate = 2021-12-12,
+                                endDate = 2021-12-13)
+
+        yield nonProgramEvent
+        nonProgramEvent.delete_instance()
 
 @pytest.mark.integration
 def test_studentled_events(training_events):
@@ -33,16 +66,23 @@ def test_studentled_events(training_events):
 
     assert allStudentLedProgram == getStudentLedEvents(2)
 
+@pytest.mark.integration
+def test_training_events(training_events):
+    training = training_events
+    allTrainingPrograms = [Event.get_by_id(1), training.event]
 
-# @pytest.mark.integration
-# def test_training_events(training_events):
-#
-#
-# @pytest.mark.integration
-# def test_bonner_events(training_events):
-#
-#
-#
-# @pytest.mark.integration
-# def test_nonProgram_events(training_events):
-#
+    assert allTrainingPrograms == getTrainingEvents(2)
+
+@pytest.mark.integration
+def test_bonner_events(special_bonner):
+    bonner = special_bonner
+    allBonnerProgram = [bonner.event]
+
+    assert allBonnerProgram == getBonnerEvents(2)
+
+@pytest.mark.integration
+def test_nonProgram_events(special_nonProgram):
+    nonProgram = special_nonProgram
+    allNonProgram = [Event.get_by_id(11), Event.get_by_id(13), nonProgram]
+
+    assert allNonProgram == getNonProgramEvents(2)
