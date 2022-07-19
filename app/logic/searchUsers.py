9a0d5bd5ac3a0dc7feed13bb2cda133014257f37
Brian Ramsay <brian@foont.net>
2022-07-18 22:37:18 -0400

    Refactor search users backend

--- app/logic/searchUsers.py
+++ app/logic/searchUsers.py
@@ -1,33 +1,27 @@
 from playhouse.shortcuts import model_to_dict
 from app.models.user import User
 def searchUsers(query, category=None):
-    '''Accepts user input and queries the database returning results that matches user search'''
-    query = query.strip()
-    search = query.upper()
-    splitSearch = search.split()
-    resultsDict = {}
+    '''
+        Search the User table based on the search query and category
 
+        MySQL LIKE is case insensitive
+    '''
+
+    # add wildcards to each piece of the query 
+    splitSearch = query.strip().split()
     firstName = splitSearch[0] + "%"
     lastName = " ".join(splitSearch[1:]) +"%"
 
-    if len(splitSearch) == 1: #search for first or last name
-        if category =="instructor":
-            results = User.select().where( User.isFaculty & (User.firstName ** firstName | User.lastName ** firstName))
-        else:
-            results = User.select().where(User.isStudent & (User.firstName ** firstName | User.lastName ** firstName))
-        for participant in results:
-            if participant not in resultsDict:
-                resultsDict[participant.username]= model_to_dict(participant)
-    else:
-        for searchTerm in splitSearch: #searching for both first and last names
-            if len(searchTerm) > 1:
-                searchTerm += "%"
-                if category =="instructor":
-                    results = User.select().where( User.isFaculty & (User.firstName ** firstName & User.lastName ** lastName))
-                else:
-                    results = User.select().where(User.isStudent & (User.firstName ** firstName & User.lastName ** lastName))
-                for participant in results:
-                    if participant not in resultsDict:
-                        resultsDict[participant.username]=model_to_dict(participant)
+    if len(splitSearch) == 1: # search for query in first OR last name
+        searchWhere = (User.firstName ** firstName | User.lastName ** firstName)
+    else:                     # search for first AND last name
+        searchWhere = (User.firstName ** firstName & User.lastName ** lastName)
 
-    return resultsDict
+    if category == "instructor":
+        userWhere = (User.isFaculty | User.isStaff)
+    else:
+        userWhere = (User.isStudent)
+
+    searchResults = User.select().where(searchWhere, userWhere)
+
+    return { user.username : model_to_dict(user) for user in searchResults }
