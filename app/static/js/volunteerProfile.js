8041f071b43f0d41746785ee92d68d7d0afba8db
bledsoef <bledsoef@berea.edu>
2022-07-20 13:36:57 -0400

    Made flash messages show and not be wiped away.

--- app/static/js/volunteerProfile.js
+++ app/static/js/volunteerProfile.js
@@ -11,13 +11,11 @@ $(document).ready(function(){
       method: "POST",
       url: interestUrl,
       success: function(response) {
-          msgFlash("Your interest has been updated", "success");
-          location.reload(true);  //  Reloading page after user clicks on the show interest checkbox
+          location.reload();  //  Reloading page after user clicks on the show interest checkbox
       },
       error: function(request, status, error) {
         console.log(status,error);
-        msgFlash("Error Updating Interest", "danger");
-        location.reload(true);
+        location.reload();
       }
     });
   });
