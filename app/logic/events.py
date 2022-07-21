d31dc34f08e6b5b3ec1c4d648b325cff7246ebe4
Luis Riera <rieral@berea.edu>
2022-07-21 13:47:57 -0400

    Search bar shows up without it needing to be editable, got rid of unnecessary line.

--- app/logic/events.py
+++ app/logic/events.py
@@ -339,7 +339,6 @@ def getFacilitatorsFromList(facilitatorList):
     """
     if facilitatorList and type(facilitatorList) == str:
         facilitatorList = facilitatorList.split(',')
-    finalFacilitatorList = []
     # for each facilitator in facilitatorList, if it is not a blank string of null
     # add a user object matching said facilitator to finalFacilitatorList
     finalFacilitatorList = [User.get_by_id(facilitator) for facilitator in facilitatorList if facilitator]
