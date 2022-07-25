dd4b3c2584ceff8557f98985b5b6b8288f08bf8a
nartehi <nartehi@berea.edu>
2022-07-25 11:11:40 -0400

    centered flash messages on admin management page and  on other pages

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,12 +1,12 @@
 function msgFlash(flash_message, status){
     if (status === "success") {
         category = "success";
-        $("#flash_container").prepend('<div class=\"w-50 position-absolute top-0 start-50 translate-middle-x alert alert-'+ category + '\" role="alert" id="flasher">' + flash_message + '</div>');
+        $("#flash_container").prepend('<div class=\"w-50 position-relative top-0 start-50 translate-middle-x alert alert-'+ category + '\" role="alert" id="flasher">' + flash_message + '</div>');
         $("#flasher").delay(5000).fadeOut();
     }
     else {
         category = "danger";
-        $("#flash_container").prepend("<div class=\"w-50 position-absolute top-0 start-50 translate-middle-x alert alert-"+ category +"\" role=\"alert\" id=\"flasher\">"+flash_message+"</div>");
+        $("#flash_container").prepend("<div class=\"w-50 position-relative top-0 start-50 translate-middle-x alert alert-"+ category +"\" role=\"alert\" id=\"flasher\">"+flash_message+"</div>");
         $("#flasher").delay(5000).fadeOut();
     }
 
