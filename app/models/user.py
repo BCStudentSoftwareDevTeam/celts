6b0a418758288a446bbb740d1f8f0e69fb121429
Andersonstettner <andersonwcstettner@gmail.com>
2022-07-26 10:27:16 -0400

    Added a isBonnerScholar flag in the user model and used it to hide the Bonner Programs tab from normal students.

--- app/models/user.py
+++ app/models/user.py
@@ -2,17 +2,18 @@ from app.models import *
 
 
 class User(baseModel):
-    username = CharField(primary_key=True)
-    bnumber = CharField(unique=True)
+    username = CharField(primary_key = True)
+    bnumber = CharField(unique = True)
     email = CharField()
-    phoneNumber = CharField(null=True)
+    phoneNumber = CharField(null = True)
     firstName = CharField()
     lastName  = CharField()
-    isStudent = BooleanField(default=False)
-    isFaculty = BooleanField(default=False)
-    isStaff = BooleanField(default=False)
-    isCeltsAdmin = BooleanField(default=False)
-    isCeltsStudentStaff = BooleanField(default=False)
+    isStudent = BooleanField(default = False)
+    isFaculty = BooleanField(default = False)
+    isStaff = BooleanField(default = False)
+    isCeltsAdmin = BooleanField(default  =False)
+    isCeltsStudentStaff = BooleanField(default = False)
+    isBonnerScholars = BooleanField(default = False)
 
     @property
     def isAdmin(self):
