743933320de1f19c294eaecef92ce6805194ae76
Luis Riera <rieral@berea.edu>
2022-08-02 14:07:51 -0400

    Rolled back our bad merge conflict solution to what is in development.

--- app/logic/courseManagement.py
+++ app/logic/courseManagement.py
@@ -50,36 +50,29 @@ def updateCourse(courseData):
         This function will take in courseData for the SLC proposal page and a dictionary
         of instuctors assigned to the course and update the information in the db.
     """
-    try:
-        course= Course.get_by_id(courseData['courseID'])
-        for toggler in ["regularOccurenceToggle", "slSectionsToggle", "permanentDesignation"]:
-            courseData.setdefault(toggler, "off")
-
-        Course.update(
-            courseName=courseData["courseName"],
-            courseAbbreviation=courseData["courseAbbreviation"],
-            courseCredit=courseData["credit"],
-            isRegularlyOccuring=("on" in courseData["regularOccurenceToggle"]),
-            term=courseData['term'],
-            status=CourseStatus.SUBMITTED,
-            isAllSectionsServiceLearning=("on" in courseData["slSectionsToggle"]),
-            serviceLearningDesignatedSections=courseData["slDesignation"],
-            isPermanentlyDesignated=("on" in courseData["permanentDesignation"]),
-        ).where(Course.id == course.id).execute()
-        for i in range(1, 7):
-            (CourseQuestion.update(questionContent=courseData[f"{i}"])
-                        .where((CourseQuestion.questionNumber == i) &
-                               (CourseQuestion.course==course)).execute())
-
-        instructorList = []
-        if 'instructor[]' in courseData:
-            instructorList = courseData.getlist('instructor[]')
-
-        CourseInstructor.delete().where(CourseInstructor.course == course).execute()
-        for instructor in instructorList:
-            CourseInstructor.create(course=course, user=instructor)
-        createLog(f"Edited SLC proposal: {courseData['courseName']}")
-    except:
-        return False;
-
+    course= Course.get_by_id(courseData['courseID'])
+    for toggler in ["regularOccurenceToggle", "slSectionsToggle", "permanentDesignation"]:
+        courseData.setdefault(toggler, "off")
+    Course.update(
+        courseName=courseData["courseName"],
+        courseAbbreviation=courseData["courseAbbreviation"],
+        courseCredit=courseData["credit"],
+        isRegularlyOccuring=("on" in courseData["regularOccurenceToggle"]),
+        term=courseData['term'],
+        status=CourseStatus.SUBMITTED,
+        isAllSectionsServiceLearning=("on" in courseData["slSectionsToggle"]),
+        serviceLearningDesignatedSections=courseData["slDesignation"],
+        isPermanentlyDesignated=("on" in courseData["permanentDesignation"]),
+    ).where(Course.id == course.id).execute()
+    for i in range(1, 7):
+        (CourseQuestion.update(questionContent=courseData[f"{i}"])
+                    .where((CourseQuestion.questionNumber == i) &
+                           (CourseQuestion.course==course)).execute())
+    instructorList = []
+    if 'instructor[]' in courseData:
+        instructorList = courseData.getlist('instructor[]')
+    CourseInstructor.delete().where(CourseInstructor.course == course).execute()
+    for instructor in instructorList:
+        CourseInstructor.create(course=course, user=instructor)
+    createLog(f"Created SLC proposal: {courseData['courseName']}")
     return Course.get_by_id(course.id)
