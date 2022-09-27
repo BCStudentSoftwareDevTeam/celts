09f728f27734920903ea7c7fc4d39b61df70df22
Brian Ramsay <ramsayb2@berea.edu>
2022-09-27 14:39:59 -0400

    UI working for requirements list (but not saving)

--- app/static/js/bonnerManagement.js
+++ app/static/js/bonnerManagement.js
@@ -1,34 +1,62 @@
 import searchUser from './searchUser.js'
 
 $(document).ready(function(){
-  $("input[type=search]").on("input", function(){
-      let year = $(this).data('year')
-      searchUser(this.id, s => submitRequest(year, "add", s.username), false, null, "student")
-  });
-  $(".removeBonner").on("click",function(){
-    let year = $(this).data('year')
-    submitRequest(year, "remove", $(this).data("username"));
-  });
-
-  // set up sortable requirements list
-    $(".frequency-select").change(function () {
-        if(!$(this).val()) {
-            $(this).addClass("empty");
-        } else {
-            $(this).removeClass("empty");
-        }
+    $("input[type=search]").on("input", function(){
+        let year = $(this).data('year')
+        searchUser(this.id, s => cohortRequest(year, "add", s.username), false, null, "student")
     });
-    $(".frequency-select").change();
+    $(".removeBonner").on("click",function(){
+        let year = $(this).data('year')
+        cohortRequest(year, "remove", $(this).data("username"));
+    });
 
-    // add sorting
+    // Add requirements sorting
+    // https://github.com/SortableJS/Sortable
+    // https://sortablejs.github.io/Sortable/
     new Sortable($('#requirements tbody')[0], {
         animation: 150,
+        forceFallback: false,
         handle: '.drag-handle',
-        ghostClass: 'sortable-ghost'
+        revertOnSpill: true,
+        onUpdate: function() {
+            enableSave();
+        }
+    });
+
+    addRequirementsRowHandlers()
+
+    // Add Row handler
+    $("#reqAdd").click(function() {
+        var table = $("#requirements");
+        var newrow = table.find("tbody tr:last-child").clone()
+        newrow.find("input").val("")
+
+        newrow.find("select.frequency-select option:first-child").attr('selected', true);
+        newrow.find("select:not(.frequency-select) option:last-child").attr('selected', true);
+
+        table.append(newrow)
+        addRequirementsRowHandlers()
+        newrow.find("input").focus()
+
+        enableSave();
+    });
+
+    // Save Requirements handler
+    $("#reqSave").click(function() {
+        disableSave();
+        $.ajax({
+        });
     });
 });
 
-function submitRequest(year, method, username){
+function enableSave() {
+    $("#reqSave").attr("disabled", false);
+}
+function disableSave() {
+    $("#reqSave").attr("disabled", true);
+}
+
+function cohortRequest(year, method, username){
   $.ajax({
     url: `/bonner/${year}/${method}/${username}`,
     type: "POST",
@@ -40,3 +68,22 @@ function submitRequest(year, method, username){
     }
   })
 }
+
+function addRequirementsRowHandlers() {
+    // Make the select have a selectable 'empty' value    
+    $(".frequency-select").change(function () {
+        if(!$(this).val()) {
+            $(this).addClass("empty");
+        } else {
+            $(this).removeClass("empty");
+        }
+    });
+    $(".frequency-select").change();
+
+    // enable the remove button
+    $("#requirements button").click(function(e) {
+        enableSave();
+        $(e.target.closest('tr')).fadeOut(function() { this.remove() });
+    });
+}
+
