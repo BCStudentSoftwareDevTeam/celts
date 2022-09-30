0b862a0b80d489405cc27fd2fc69dcf671bdd6d3
Brian Ramsay <ramsayb2@berea.edu>
2022-09-30 15:25:40 -0400

    PR feedback

--- app/logic/certification.py
+++ app/logic/certification.py
@@ -15,16 +15,16 @@ def getCertRequirements(certification=None):
         A list of dictionaries with all certification data and requirements. If `certification`
         is given, returns only a list of requirement objects for the given certification.
     """
-    query = (Certification.select(Certification, CertificationRequirement)
+    reqList = (Certification.select(Certification, CertificationRequirement)
                          .join(CertificationRequirement, JOIN.LEFT_OUTER, attr="requirement")
                          .order_by(Certification.id, CertificationRequirement.order.asc(nulls="LAST")))
     if certification:
         # we have to add the is not null check so that `cert.requirement` always exists
-        query = query.where(Certification.id == certification, CertificationRequirement.id.is_null(False))
-        return [cert.requirement for cert in query]
+        reqList = reqList.where(Certification.id == certification, CertificationRequirement.id.is_null(False))
+        return [cert.requirement for cert in reqList]
     
     certs = {}
-    for cert in query:
+    for cert in reqList:
         if cert.id not in certs.keys():
             certs[cert.id] = {"data": cert, "requirements": []}
 
