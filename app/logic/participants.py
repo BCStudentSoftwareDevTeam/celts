e37c27e84a1b42a43a3c68d717c68e6687353f62
Luis Riera <rieral@berea.edu>
2022-10-13 15:26:53 -0400

    Started to fix errors in the tests

--- app/logic/participants.py
+++ app/logic/participants.py
@@ -20,14 +20,24 @@ def trainedParticipants(programID, currentTerm):
 
     # Reset program eligibility each term for all other trainings
 
+    # otherTrainingEvents = (Event.select(Event.id)
+    #         .join(ProgramEvent).switch()
+    #         .join(Term)
+    #         .where(
+    #             ProgramEvent.program == programID,
+    #             (Event.isTraining | Event.isAllVolunteerTraining),
+    #             Event.term.academicYear == academicYear)
+    #         )
+    otherCeltsSponsoredEvents = Program.get_by_id(11)
     otherTrainingEvents = (Event.select(Event.id)
-            .join(ProgramEvent).switch()
-            .join(Term)
-            .where(
-                ProgramEvent.program == programID,
-                (Event.isTraining | Event.isAllVolunteerTraining),
-                Event.term.academicYear == academicYear)
-            )
+                            .join(Term)
+                            .where(
+                                    (Event.program == programID) | (Event.program == otherCeltsSponsoredEvents),
+                                    (Event.isTraining) | (Event.isAllVolunteerTraining),
+                                    Event.term.academicYear == academicYear
+                                    )
+
+                          )
 
     allTrainingEvents = set(otherTrainingEvents)
     eventTrainingDataList = [participant.user for participant in (
