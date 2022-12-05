f230a0d4c6b757de97224d67df8262417f042b40
Brian Ramsay <brian@foont.net>
2022-12-05 12:55:53 -0500

    Back up and scrub celts db

--- database/data-scrub.sql
+++ database/data-scrub.sql
@@ -7,7 +7,7 @@ UPDATE backgroundcheck SET backgroundCheckStatus=CASE WHEN RAND() < .05 THEN 'Fa
 DELETE FROM eventfile
 
 -- Remove comments and notes
-UPDATE notes set notesContent='Notes are not visible except in the production environment'
+UPDATE note set noteContent='Notes are not visible except in the production environment'
 
 -- randomize program ban users
 UPDATE programban set user_id=(SELECT username from user ORDER BY RAND() LIMIT 1)
