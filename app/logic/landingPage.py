f6edca954563436ecab608ac4fa55b8291bd763b
Andersonstettner <stettnera30@gmail.com>
2023-01-31 10:19:53 -0500

    If there is no Program Manager for a program then the manager is left blank on the program card on the landing page.

--- app/logic/landingPage.py
+++ app/logic/landingPage.py
@@ -13,14 +13,14 @@ def getManagerProgramDict(user):
     managerProgramDict = {}
 
     for program in programs:
-        managerProgramDict[program] = {"managers": "Nobody", "image": os.path.join('static', 'images/logos/celts_symbol.png')}
+        managerProgramDict[program] = {"managers": "", "image": os.path.join('static', 'images/logos/celts_symbol.png')}
         with os.scandir("./app/static/images/landingPage") as it:
             for entry in it:
                 if entry.name.split('.')[0] == f'{program.programName}':
                     managerProgramDict[program]["image"] = os.path.join('static', f'images/landingPage/{entry.name}')
                     break
     for row in managerRows:
-        if managerProgramDict[row.program]["managers"] == "Nobody":
+        if managerProgramDict[row.program]["managers"] == "":
             managerProgramDict[row.program]["managers"] = f'{row.user.firstName} {row.user.lastName}'
         else:
             managerProgramDict[row.program]["managers"] = f'{managerProgramDict[row.program]["managers"]}, {row.user.firstName} {row.user.lastName}'
