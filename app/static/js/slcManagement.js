ca8f8fe4a17faadd73571012961cdfccb77ed9b1
bledsoef <bledsoef@berea.edu>
2023-02-22 09:39:18 -0500

    merged javascript files and added print to admin course management dropdown

--- app/static/js/slcManagement.js
+++ app/static/js/slcManagement.js
@@ -23,6 +23,8 @@ function changeAction(action){
     location = '/serviceLearning/editProposal/' + courseID;
   } else if(action.value == "Print"){
     slcPrintPDF(courseID)
+  } else if (action.value == "Review"){
+    reviewCourses(courseID)
   }
 }
 function renew(){
@@ -59,4 +61,49 @@ function slcPrintPDF(courseID){
   printProposal.onafterprint = function(){
     printProposal.close()
   }
+}
+
+function changeTerm() {
+  $('form').submit();
+};
+
+function formSubmit(el) {
+$("#termSelector").attr('action', '/manageServiceLearning/' + el);
+$("#termSelector").submit()
+};
+
+function reviewCourses(courseID) {
+$.ajax({
+  url: "/proposalReview/",
+  type: "POST",
+  data: {"course_id":courseID},
+  success: function(modal_html) {
+    $("#review-modal").html(modal_html)
+    $("#proposal_view").modal('show')
+  }
+})
+}
+
+function approveProposal(el){
+let courseID = $(el).data("id")
+$.ajax({
+  url: '/serviceLearning/approveCourse',
+  type: "POST",
+  data: {"courseID":courseID},
+  success: function(){
+    location.reload()
+  }
+})
+}
+
+function unapproveProposal(el){
+let courseID = $(el).data("id")
+$.ajax({
+  url: '/serviceLearning/unapproveCourse',
+  type: "POST",
+  data: {"courseID":courseID},
+  success: function(){
+    location.reload()
+  }
+})
 }
