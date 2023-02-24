9e3eaa8a59aa0da5f61fe99392f0a9fec366ac90
Sreynit Khatt <khatts@berea.edu>
2023-02-24 17:28:48 -0500

    greyed out students who unrsvp

--- app/static/js/trackVolunteers.js
+++ app/static/js/trackVolunteers.js
@@ -128,14 +128,4 @@ $(document).ready(function() {
     })
   }
 
-$("#trackVolunteerBtn").click(function() {
-  var event = $("#volunteerTable").attr("name");
-  if (event.unRsvpTime > event.rsvpTime) {
-    console.log("nopee");
-  } else {
-    console.log("yeppp");
-  }
-  // console.log(event);
-});
-
 })
