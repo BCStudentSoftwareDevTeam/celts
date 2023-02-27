8575c1e8e98fa14ee78b5f96cfdf90b9aa327314
thawpt <thawp@berea.edu>
2023-02-27 16:30:03 -0500

    edited location

--- app/static/js/slcManagement.js
+++ app/static/js/slcManagement.js
@@ -10,10 +10,7 @@ $(document).ready(function() {
 
 function changeAction(action){
   courseID = action.id;
-  console.log("courseID value:", courseID);
-  var courseSt = parseInt($('#slcNewProposal').attr('coursestatus'));
-  console.log("courseSt value:", courseSt);
-  console.log("APPROVED value:", 3);
+  var courseSt = $("#courseStatus-" + courseID).html();
   // decides what to do based on selection
   if (action.value == "Renew"){
     $('#courseID').val(courseID);
@@ -24,13 +21,12 @@ function changeAction(action){
     $('#courseID').val(courseID);
     $('#withdrawModal').modal('show');
   } else if(action.value == "Edit"){
-      if(courseSt == 3){
-        console.log("viewProposal");
-        location = '/serviceLearning/viewProposal/' + courseID
+      if(courseSt == "Approved"){
+        window.location.href = "/serviceLearning/viewProposal/" + courseID;
+        return;
       }
       else {
-        console.log("Redirecting to editProposal");
-        location = '/serviceLearning/editProposal/' + courseID;
+        location = '/serviceLearning/editProposal/' + courseID
       }
     }
 }
