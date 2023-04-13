b09c009cab099202b979ac3872d2b400a62f9787
Fleur Gahimbare <gahimbaref@berea.edu>
2023-04-13 14:43:45 -0400

    add query for repeat volunteers

--- app/logic/spreadsheet.py
+++ app/logic/spreadsheet.py
@@ -5,6 +5,7 @@ from app.models.programEvent import ProgramEvent
 from app.models.program import Program
 from peewee import fn, JOIN
 
+
 def volunteer():
 
     # Total volunteer hours by program along with a sum of all programs
@@ -23,9 +24,9 @@ def volunteer():
 
     # Majors represented in volunteering
     major_query = (User.select(User.major, fn.COUNT(fn.DISTINCT(EventParticipant.user_id)).alias('count'))
-                        .join(EventParticipant, on=(User.username == EventParticipant.user_id))
-                        .group_by(User.major))
-
+                         .join(EventParticipant, on=(User.username == EventParticipant.user_id))
+                         .group_by(User.major))
+ 
     print("_____________majors represented___")
     for row in major_query:
         print(row.major, row.count)
@@ -33,12 +34,39 @@ def volunteer():
 
     # Volunteering numbers by class year
     classLevel_query = (User.select(User.classLevel, fn.COUNT(fn.DISTINCT(EventParticipant.user_id)).alias('classCount'))
-                        .join(EventParticipant, on=(User.username == EventParticipant.user_id))
-                        .group_by(User.classLevel))
+                         .join(EventParticipant, on=(User.username == EventParticipant.user_id))
+                         .group_by(User.classLevel))
 
     print("_____________class year___")
     for row in classLevel_query:
         print(row.classLevel, row.classCount)
 
-# Repeat volunteers (for individual events/programs and across all programs)
-# Retention rates of volunteers (waiting for a bit of clarification from CELTS, check with me if you pick up this issue)
+
+    print("_____________Repeat Volunteers___")
+    # Repeat volunteers (for individual events/programs and across all programs)
+    # Excluding all volunteer training for now
+    # Get people who came more than once for a program
+    p_query = (EventParticipant
+                         .select(   EventParticipant.user_id,
+                                    (ProgramEvent.program_id).alias('program_id'),
+                                    Program.programName.alias("programName"),
+                                    fn.Count(EventParticipant.event_id).alias('event_count'))
+                         .join(ProgramEvent, on=(EventParticipant.event_id == ProgramEvent.event_id))
+                         .join(Program, on=(ProgramEvent.program_id == Program.id))
+                         .group_by(EventParticipant.user_id, ProgramEvent.program_id)
+                         .having(fn.Count(EventParticipant.event_id) > 1))
+    for result in p_query.dicts():
+        print(f"Participant:", result["user"], "   Events:", result["event_count"], "    Program Id/Name:", result["program_id"], result["programName"])
+    print("-----------------------")
+
+    
+
+    # People who came more than twice for a program
+    # Get people/how many that participated in any to any event
+
+
+    # Retention rates of volunteers (waiting for a bit of clarification from CELTS, check with me if you pick up this issue)
+
+
+
+
