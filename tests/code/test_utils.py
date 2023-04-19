cf69462e60bc1fd2d67cdcfcd2cc9e6da11f954f
Andersonstettner <stettnera30@gmail.com>
2023-04-19 12:39:23 -0400

    moved deup_update to config test file

--- tests/code/test_utils.py
+++ tests/code/test_utils.py
@@ -3,7 +3,6 @@ import pytest
 from app.models import mainDB
 from app.models.user import User
 from app.models.term import Term
-from app.logic.config import deep_update
 from app.logic.utils import selectSurroundingTerms
 from app.logic.userManagement import addCeltsAdmin, removeCeltsAdmin,addCeltsStudentStaff, removeCeltsStudentStaff, changeCurrentTerm, addNextTerm
 
@@ -21,83 +20,6 @@ def test_selectSurroundingTerms():
 
     listOfTerms = selectSurroundingTerms(Term.get_by_id(3), prevTerms=-1)
     assert [4,5,6,7,8,9] == [t.id for t in listOfTerms]
-
-@pytest.mark.unit
-def test_deepUpdate_empty():
-    d1 = {}
-    d2 = {"a" : {"key": 7}}
-    result = {"a" : {"key": 7}}
-
-    return_val = deep_update(d1, d2)
-    assert result == d1
-    assert result == return_val
-
-    d1 = {"a" : {"key": 7}}
-    d2 = {}
-    result = {"a" : {"key": 7}}
-
-    return_val = deep_update(d1, d2)
-    assert result == d1
-    assert result == return_val
-
-    d1 = {"a" : {"key": 7}}
-    d2 = None
-    result = {"a" : {"key": 7}}
-
-    return_val = deep_update(d1, d2)
-    assert result == d1
-    assert result == return_val
-
-    # since there is no reference parameter to update, we can only check the return value
-    d1 = None
-    d2 = {"b": {"alpha" : 17}}
-    result = {"b": {"alpha" : 17}}
-
-    return_val = deep_update(d1, d2)
-    assert result == return_val
-
-
-@pytest.mark.unit
-def test_deepUpdate():
-    d1 = {"a" : 1}
-    d2 = {"b": 2, "a": 3}
-    result = {"a": 3, "b": 2}
-
-    return_val = deep_update(d1, d2)
-    assert result == d1
-    assert result == return_val
-
-    d1 = {"a" : {"key": 7}}
-    d2 = {"b": 2, "a": 3}
-    result = {"a": 3, "b": 2}
-
-    return_val = deep_update(d1, d2)
-    assert result == d1
-    assert result == return_val
-
-    d1 = {"a" : 3}
-    d2 = {"b": 2, "a": {"key": 7}}
-    result = {"a": {"key": 7}, "b": 2}
-
-    return_val = deep_update(d1, d2)
-    assert result == d1
-    assert result == return_val
-
-    d1 = {"a" : {"key": 8}}
-    d2 = {"b": 2, "a": {"key": 7}}
-    result = {"a": {"key": 7}, "b": 2}
-
-    return_val = deep_update(d1, d2)
-    assert result == d1
-    assert result == return_val
-
-    d1 = {"a" : {"key": 8}}
-    d2 = {"b": 2, "a": {"newkey": 12}}
-    result = {"a": {"key": 8, "newkey": 12}, "b": 2}
-
-    return_val = deep_update(d1, d2)
-    assert result == d1
-    assert result == return_val
 
 def test_changeCurrentTerm():
     # test via g.current_term
