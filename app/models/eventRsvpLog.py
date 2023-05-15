4dd1ebf141b097d5a016c44df88a7fe2190b78da
Andersonstettner <stettnera30@gmail.com>
2023-05-15 11:09:18 -0400

    added last field to log

--- app/models/eventRsvpLog.py
+++ app/models/eventRsvpLog.py
@@ -1,9 +1,9 @@
-from datetime import datetime
 from app.models import* 
 from app.models.user import User 
 from app.models.event import Event
 
 class eventRsvpLog(baseModel):
     createdBy = ForeignKeyField(User)
+    createdOn = DateTimeField()
     rsvpLogContent = CharField()
     event = ForeignKeyField(Event, backref="rsvpLogs")
