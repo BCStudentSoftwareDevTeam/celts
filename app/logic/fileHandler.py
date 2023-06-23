176aaf3606b8d7ac501c5603330562dfff2dccb1
None <makindeo@berea.edu>
2023-06-23 16:33:15 -0400

    Images can now be viewed across recurring events. Refactoring still remains to be done.

--- app/logic/fileHandler.py
+++ app/logic/fileHandler.py
@@ -12,46 +12,52 @@ class FileHandler:
         if courseId:
             self.path = os.path.join(self.path, app.config['files']['course_attachment_path'], str(courseId))
         elif eventId:
-            self.path = os.path.join(self.path, app.config['files']['event_attachment_path'], str(eventId))
+            self.path = os.path.join(self.path, app.config['files']['event_attachment_path'])       #Removed event ID from how the file path is being saved.
+        
         try:
-            os.makedirs(self.path)
+            extraDir = str(self.eventId) if self.eventId else ""
+            os.makedirs(os.path.join(self.path, extraDir))
         except:
             print("Directory exists.")
 
-    def getFileFullPath(self, newfile = None):
+    def getFileFullPath(self, newfilename = ''):
         """
         This creates the directory/path for the object from the "Choose File" input in the create event and edit event.
         :returns: directory path for attachment
         """
+
+        # Added the eventID of the first recurring event to track the file path for subsequent recurring events.
+
         try:
             # tries to create the full path of the files location and passes if
             # the directories already exist or there is no attachment
-            filePath=(os.path.join(self.path, newfile.filename))
+            filePath=(os.path.join(self.path, newfilename))                                  # This line adds the eventID of the first recurring event.
         except AttributeError:  # will pass if there is no attachment to save
             pass
         except FileExistsError:
             pass
+
+        print(filePath)
         return filePath
 
-    def saveFiles(self, saveOriginalFile):
-        """ Saves the attachment in the app/static/files/eventattachments/ or courseattachements/ directory """
+    def saveFiles(self, saveOriginalFile=None):
+        """ Saves the attachment in the app/static/files/eventatta chments/ or courseattachements/ directory """
         try:
-            fileCount = 1
             for file in self.files:
                 if self.eventId:
-                    if fileCount:       # Checks if iterant is the first (recurring) event
+                    if saveOriginalFile and saveOriginalFile.id == self.eventId:       # Checks if iterant is the first (recurring) event
                         isFileInEvent = AttachmentUpload.select().where(AttachmentUpload.event == self.eventId, AttachmentUpload.fileName == file.filename).exists()
                         if not isFileInEvent:
-                            AttachmentUpload.create(event = self.eventId, fileName = file.filename)
-                            file.save(self.getFileFullPath(newfile = file))
-                            fileCount = 0
-                            print("The lines are working effectively")
+                            AttachmentUpload.create(event = self.eventId, fileName = str(saveOriginalFile.id) + "/" + file.filename)
+
+                            # saves attachment in the same directory using the eventID of the first recurring event.
+                            file.save(self.getFileFullPath(newfilename = str(saveOriginalFile.id) + "/" + file.filename)) 
+                            # We need to add the event id to the file path when we call it in file.save.
                     else:
                         isFileInEvent = AttachmentUpload.select().where(AttachmentUpload.event == self.eventId, AttachmentUpload.fileName == file.filename).exists()
                         if not isFileInEvent:
-                            AttachmentUpload.create(event = self.eventId, fileName = file.filename)
-                            file.save(self.getFileFullPath(newfile = file))
-                            print("Code not working, help")
+                            AttachmentUpload.create(event = self.eventId, fileName = str(saveOriginalFile.id) + "/" + file.filename)
+                            
                 elif self.courseId:
                     isFileInCourse = AttachmentUpload.select().where(AttachmentUpload.course == self.courseId, AttachmentUpload.fileName == file.filename).exists()
                     if not isFileInCourse:
