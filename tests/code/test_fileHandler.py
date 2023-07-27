c154ce84b6b66ffdf6d1c6dff77fa91d8961333b
Mercy Eze <ezem@berea.edu>
2023-07-27 15:59:44 -0400

    Restored Test

--- tests/code/test_fileHandler.py
+++ tests/code/test_fileHandler.py
@@ -41,7 +41,7 @@ def test_makingdirectory():
     event_id = 90
     path = 'app/static/files/eventattachments/'
     # Ensure the directory does not exist before calling makeDirectory
-    assert os.path.exists(os.path.join(path, str(event_id))) == True
+    assert os.path.exists(os.path.join(path, str(event_id))) == False
     
     # Creating directory and making sure it exist
     eventFileStorage= [FileStorage(filename= "eventfile.pdf")]
