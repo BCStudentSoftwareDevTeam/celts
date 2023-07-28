6a3227e8b36ca46ef41f385bcc4caf3a694ed169
Liliane Azabe <azabel@berea.edu>
2023-07-28 16:32:17 -0400

    changed the display and added table option depending on what the user is looking for,added the taggle button to let the user choose either the card view or table view

--- app/static/js/volunteerInformation.js
+++ app/static/js/volunteerInformation.js
@@ -1,31 +1,49 @@
 $(document).ready(function () {
+	$("#volunteerInformationTableToPrint").hide()
+	$("#tableCardToggle").on('change', function () {
+		$("#volunteerInformationCardToPrint").toggle()
+		$("#volunteerInformationTableToPrint").toggle()
+
+		
+		if ($("#tableCardToggleLabel").text() == "Card View") {
+			$("#tableCardToggleLabel").text("Table View")
+		} else {
+			$("#tableCardToggleLabel").text("Card View")
+		}
+			
+		console.log($("#tableCardToggleLabel").text())
+		updateTable()
+	})
 	$('#printVolunteerInfo').on('click', function () {
-		$('#volunteerInformationToPrint').siblings().hide()
-		$('#volunteerInformationToPrint').parents().siblings().hide()
-		$('#volunteerInformationToPrint').css('column-count', '2');
+		let contentToPrint, contentToHide
+		if ($('#tableCardToggle').is(':checked')) {
+			contentToPrint = $("#volunteerInformationTableToPrint")
+			contentToHide = $("#volunteerInformationCardToPrint")
+		} else {
+			contentToPrint = $("#volunteerInformationCardToPrint")
+			contentToHide = $("#volunteerInformationTableToPrint")
+		}
+		contentToPrint.siblings().hide()
+		contentToPrint.parents().siblings().hide()
+		if (contentToPrint.length > 1) {
+			contentToPrint.css('column-count', '2');	
+		} 
 		window.print()
-		$('#volunteerInformationToPrint').siblings().show()
-		$('#volunteerInformationToPrint').parents().siblings().show()
-		$('#volunteerInformationToPrint').css('column-count', '1');
+		contentToPrint.siblings().show()
+		contentToPrint.parents().siblings().show()
+		contentToPrint.css('column-count', '1');
+		contentToHide.hide()
 		
 		
 	})
-	$(".displayCheckbox").on('change', function(){
-		$(".displayCheckbox").each(function() {
-			let checkboxId = this.id;
-			if ($('#' + checkboxId).is(':checked')) {
-				$("." + checkboxId).show()
-			} 
-			else {
-				$("." + checkboxId).hide()
-			}
-		})
+	$(".displayCheckbox").on('change', function () {
+		getCheckBoxes()
 		updateTable()
 	})
 	function updateTable() {
-		let allEntries = $("#volunteerInformationToPrint .volunteerInfoEntries")
-		let shownUsers = [] 
-		for (let i=0; i < allEntries.length; i++) {
+		let allEntries = $(".volunteerInfoEntries")
+		let shownUsers = []
+		for (let i = 0; i < allEntries.length; i++) {
 			let currentEntry = $(allEntries[i])
 			if (!currentEntry.is(":hidden")) {
 				if (shownUsers.includes(currentEntry.data("user"))) {
@@ -34,9 +52,22 @@ $(document).ready(function () {
 				else {
 					shownUsers.push(currentEntry.data("user"))
 				}
-				console.log(shownUsers)
+
 			}
 		}
 	}
+	
+	function getCheckBoxes() {
+		$(".displayCheckbox").each(function () {
+			let checkboxId = this.id;
+			if ($('#' + checkboxId).is(':checked')) {
+				$("." + checkboxId).show()
+			}
+			else {
+				$("." + checkboxId).hide()
+			}
+		})
+	}
+	getCheckBoxes()
 	updateTable()
 })
