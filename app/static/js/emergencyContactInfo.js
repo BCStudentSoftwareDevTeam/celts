3c10970fc000735c4f949451c844878d52f85357
hoerstl <hoerstl@berea.edu>
2023-08-02 12:00:16 -0400

    Added number and form verification to both pages

--- app/static/js/emergencyContactInfo.js
+++ app/static/js/emergencyContactInfo.js
@@ -1,16 +1,40 @@
 $(document).ready(function(){
 
+    $('input.phone-input').on('input', function(){
+        let matches = $(this).val().match(/\d/g);
+        let digits = matches?matches.length:0;
+        if (digits == 0 || digits == 10){
+            this.setCustomValidity('')
+        }
+        else{
+            this.setCustomValidity('Please Enter a Valid Phone Number')    
+            this.reportValidity()        
+        }
+    })
+
     $('#saveExit').on('click', function(){
         let username = $('#username').attr('value')
         $('#contactForm').prop('action', `/profile/${username}/emergencyContact?action=exit`)
-        $('#contactForm').submit()
+        if (formIsValid()){
+            $('#contactForm').submit()
+        }
     })
 
     $('#saveContinue').on('click', function(){
         let username = $('#username').attr('value')
         $('#contactForm').prop('action', `/profile/${username}/emergencyContact?action=continue`)
-        $('#contactForm').submit()
+        if (formIsValid()){
+            $('#contactForm').submit()
+        }
     })
-
     
+    function formIsValid(){
+        let invalidInputs = $('input').map(function(i, e){
+            if (! e.checkValidity()) {
+                e.reportValidity()
+                return e
+            }
+        })
+        return invalidInputs.length == 0
+    }
 });
--- app/static/js/insuranceInfo.js
+++ app/static/js/insuranceInfo.js
@@ -3,14 +3,26 @@ $(document).ready(function(){
     $('#saveExit').on('click', function(){
         let username = $('#username').attr('value')
         $('#insuranceForm').prop('action', `/profile/${username}/insuranceInfo?action=exit`)
-        $('#insuranceForm').submit()
+        if (formIsValid()){
+            $('#insuranceForm').submit()
+        }
     })
 
     $('#saveContinue').on('click', function(){
         let username = $('#username').attr('value')
         $('#insuranceForm').prop('action', `/profile/${username}/insuranceInfo?action=continue`)
-        $('#insuranceForm').submit()
+        if (formIsValid()){
+            $('#insuranceForm').submit()
+        }
     })
 
-    
+    function formIsValid(){
+        let invalidInputs = $('input').map(function(i, e){
+            if (! e.checkValidity()) {
+                e.reportValidity()
+                return e
+            }
+        })
+        return invalidInputs.length == 0
+    }
 });
