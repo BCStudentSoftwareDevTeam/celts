b830e6491e4fb9fab871210ca4d7ed06fb5a68e5
Brian Ramsay <ramsayb2@berea.edu>
2023-08-07 13:44:00 -0400

    Fixed boneheaded table id

--- app/static/js/manageServiceLearningFaculty.js
+++ app/static/js/manageServiceLearningFaculty.js
@@ -1,17 +1,15 @@
 $(document).ready( function () {
 
   //make html table to datatable
-  var table = $('#SLCProposalTable').DataTable({
+  var table = $('#SLCFacultyTable').DataTable({
    "fnDrawCallback": function(oSettings) {
-      if ($('#SLCProposalTable tr').length <= 10) {
+      if ($('#SLCFacultyTable tr').length <= 10) {
          // if entries are less than or equal to 10, there is no need for the dropdown with entries to show.
          $('.dataTables_length').hide();
          //move search box to the left
          $('.dataTables_filter').addClass('float-none');
        }
     }
-    
-
   });
 
   $('#modalPreview button[data-bs-dismiss="modal"]').click(function () {
