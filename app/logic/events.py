00b975fb3efe846cb33c2c8604443f20cd863b6b
Andersonstettner <stettnera30@gmail.com>
2023-08-07 11:36:07 -0400

    fixed broken test

--- app/logic/events.py
+++ app/logic/events.py
@@ -32,10 +32,8 @@ def cancelEvent(eventId):
         event.save()
 
     program = event.program
-    if program:
-        createAdminLog(f"Canceled <a href= \"{url_for('admin.eventDisplay', eventId = event)}\" >{event.name}</a> for {program.programName}, which had a start date of {datetime.datetime.strftime(event.startDate, '%m/%d/%Y')}.")
-    else:
-        createAdminLog(f"Canceled a non-program event, <a href= \"{url_for('admin.eventDisplay', eventId = event)}\" >{event.name}</a>, which had a start date of {datetime.datetime.strftime(event.startDate, '%m/%d/%Y')}.")
+    createAdminLog(f"Canceled <a href= \"{url_for('admin.eventDisplay', eventId = event)}\" >{event.name}</a> for {program.programName}, which had a start date of {datetime.datetime.strftime(event.startDate, '%m/%d/%Y')}.")
+
 
 def deleteEvent(eventId):
     """
