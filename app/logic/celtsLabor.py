de08d33d4b19ef2138acac272faae74e9edd3993
Andersonstettner <stettnera30@gmail.com>
2023-09-18 15:33:53 -0400

    added test for logic function.

--- app/logic/celtsLabor.py
+++ app/logic/celtsLabor.py
@@ -3,22 +3,9 @@ import json
 
 def callLsfApi(bNumber):
     """
-    Make a call to the LSF endpoint which returns every students labor info. 
+    Make a call to the LSF endpoint which returns a specific students labor information.  
 
-    The data is a dictionary with keys that are B#s and values that are lists of dicts which contain labor information
-    {"B#":[{"jobType":"Primary",
-            "laborEnd":"Mon, 18 Jul 2016 00:00:00 GMT",
-            "laborStart":"Mon, 23 May 2016 00:00:00 GMT",
-            "positionTitle":"Habitat for Humanity Coord.",
-            "termCode":201513,
-            "termName":"Summer 2016",
-            "wls":"5"},
-            {"jobType":"Primary",
-            "laborEnd":"Tue, 13 Dec 2016 00:00:00 GMT","
-            laborStart":"Tue, 23 Aug 2016 00:00:00 GMT",
-            "positionTitle":"Habitat for Humanity Coord.",
-            "termCode":201611,
-            "termName":"Fall 2016","wls":"5"}]
+    The returned data is a dictionary with B# key and value that is a list of dicts that contain the labor information. 
     """
     try: 
         lsfUrl = f"http://172.31.2.114:8080/api/usr/{bNumber}"
---
+++ tests/code/test_celtsLabor.py
@@ -0,0 +1,39 @@
+import pytest 
+
+from app.models.user import User
+from app.logic.celtsLabor import getPositionAndTerm
+from app.logic import celtsLabor
+
+def demoCallLsfApi(bnumber): 
+    fakeLsfData = {"B00751864":[{"positionTitle": "Fake Position",
+                                  "termCode": "202000",
+                                  "laborStart": 12-13-2020,
+                                  "laborEnd": 12-13-2021,
+                                  "jobType": "Primary", 
+                                  "wls": "1", 
+                                  "termName": "AY 2020-2021"},
+                                  {"positionTitle": "Fake Position",
+                                  "termCode": "202000",
+                                  "laborStart": 12-13-2021,
+                                  "laborEnd": 12-13-2022,
+                                  "jobType": "Primary", 
+                                  "wls": "1", 
+                                  "termName": "AY 2021-2022"},
+                                  {"positionTitle": "Fake Position But a Leader",
+                                  "termCode": "202000",
+                                  "laborStart": 12-13-2022,
+                                  "laborEnd": 12-13-2023,
+                                  "jobType": "Primary", 
+                                  "wls": "1", 
+                                  "termName": "AY 2022-2023"}]
+                                }
+    return fakeLsfData
+
+celtsLabor.callLsfApi = demoCallLsfApi
+
+@pytest.mark.integration
+def test_getPositionAndTerm():
+    volunteer = User.get(User.username == "neillz")
+    neillzPositionAndTermInfo = getPositionAndTerm(volunteer)
+    assert neillzPositionAndTermInfo == {'Fake Position': ['AY 2020-2021', 'AY 2021-2022'], 
+                                         'Fake Position But a Leader': ["AY 2022-2023"]}
