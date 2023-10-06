5bfabe598a62d00b55ab9b23f8a755d2ff8d4078
Fleur Gahimbare <gahimbaref@berea.edu>
2023-10-06 17:03:20 -0400

    fix the wording)

--- app/static/js/event_list.js
+++ app/static/js/event_list.js
@@ -77,20 +77,20 @@ function eventCountDown(eventDateTime) {
     var countdownText = '';
 
     if (days > 1) {
-      countdownText = "is in " + days + " days";
+      countdownText = "(in " + days + " days)";
     } else if (days === 1) {
-      countdownText = "is happening tomorrow";
+      countdownText = "(tomorrow)";
     } else if (days === 0) {
       if (hours > 0) {
-        countdownText = "is happening today " + hours + " hours and " + minutes + " minutes.";
+        countdownText = "(in " + hours + " hours and " + minutes + " mins)";
       } else {
-        countdownText = "is happening today in " + minutes + " minutes.";
+        countdownText = "(in " + minutes + " mins)";
       }
     }
 
     $("#countdown-text").text(countdownText);
   } else {
-    var countdownText = "has already passed";
+    var countdownText = "(already passed)";
     $("#countdown-text").text(countdownText);
   }
 
@@ -111,47 +111,5 @@ if (ampm === "PM" && hours < 12) {
 eventDate.setHours(hours, minutes, 0, 0); // Set the event time in the eventDate
 
 eventCountDown(eventDate);
-
-
-
-// // Calculate and update the countdown
-// function eventCountDown(eventDate) {
-//   var now = new Date();
-//   var timeRemaining = eventDate - now;
-  
-
-//   if (timeRemaining > 0) {
-//     var days = Math.floor((timeRemaining / (1000 * 60 * 60 * 24))+1);
-
-//     var countdownText = '';
-
-//     if (days > 1) {
-//       countdownText = "is in " + days + " days";
-//     } else if (days === 1) {
-//       countdownText = "is happening tomorrow";
-//     } 
-
-//     $("#countdown-text").text(countdownText);
-//     if (days = 0) {  
-//     var countdownText = "is happening today!";
-//     $("#countdown-text").text(countdownText);
-//     }
-//   } else {
-//     var countdownText = "has already passed";
-//     $("#countdown-text").text(countdownText);
-//   }
-
-//   console.log(countdownText);
-// }
-
-// // Set the event date from HTML data attribute
-// var eventDate = new Date($("#countdown").data("event-date"));
-
-// // Display the event date in Eastern Time (ET)
-// var eventDateText = eventDate.toLocaleString('en-US', { timeZone: 'America/New_York' });
-// $("#event-date").text(eventDateText);
-
-// // Initial update
-// eventCountDown(eventDate);
 
 
