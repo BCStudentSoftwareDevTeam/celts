ec28a420ca8d622be1c0f51fff4db9d49c6f8067
qasema <qasema@berea.edu>
2023-10-12 16:11:18 -0400

    update program links

--- database/test_data.py
+++ database/test_data.py
@@ -372,7 +372,7 @@ programs = [
     {
         "id": 1,
         "programName": "Hunger Initiatives",
-        "programUrl": "https://www.berea.edu/celts/community-service-programs/hunger-initiatives/",
+        "programUrl": "https://instagram.com/bereacollegecelts?igshid=NzZhOTFlYzFmZQ==",
         "programDescription": "Each year 200 people stand in line to get into Woods-Penniman for the Annual Empty Bowls Event sponsored by the Berea College ceramics students and CELTS. Students, faculty, staff and community members each pay $10 for a beautiful bowl, soup and the privilege of helping those in need in our community.",
         "isStudentLed": False,
         "isBonnerScholars": False,
@@ -383,7 +383,7 @@ programs = [
     {
         "id": 2,
         "programName": "Berea Buddies",
-        "programUrl": "https://www.berea.edu/celts/community-service-programs/volunteer-opportunities/berea-buddies-program/",
+        "programUrl": "https://www.instagram.com/bereabuddies/",
         "programDescription": "The Berea Buddies program is dedicated to establishing long-term mentorships between Berea youth (Little Buddies) and Berea College students (Big Buddies). Volunteers serve children by offering them friendship and quality time. Big and Little Buddies meet each other every Monday or Tuesday during the academic year, except on school and national holidays, to enjoy structured activities around campus.",
         "isStudentLed": True,
         "isBonnerScholars": False,
@@ -395,7 +395,7 @@ programs = [
     {
         "id": 3,
         "programName": "Adopt-a-Grandparent",
-        "programUrl": "https://www.berea.edu/celts/community-service-programs/volunteer-opportunities/adopt-a-grandparent-program/",
+        "programUrl": "https://www.instagram.com/agp_celts/",
         "programDescription": "Adopt-a-Grandparent (AGP) is an outreach program for Berea elders. The program matches college student volunteers with residents of local long-term care centers. Volunteers visit with residents for at least an hour per week, and participate in special monthly programs.",
         "isStudentLed": True,
         "isBonnerScholars": False,
@@ -406,7 +406,7 @@ programs = [
     {
         "id": 4,
         "programName": "People Who Care",
-        "programUrl": "https://www.berea.edu/celts/community-service-programs/volunteer-opportunities/people-who-care-program/",
+        "programUrl": "https://www.instagram.com/pwc_bc/",
         "programDescription":"People Who Care (PWC) helps to connect Berea College students with organizations and opportunities that promote change through advocacy, education, action, and direct community service. Volunteers may serve at local shelters, work with the Fair Trade University Campaign, or help to raise awareness about local issues like domestic violence, homelessness, fair trade, and AIDS awareness education. Students are welcome to participate as volunteers in PWC’s projects.",
         "isStudentLed": True,
         "isBonnerScholars": False,
@@ -428,7 +428,7 @@ programs = [
     {
         "id": 6,
         "programName": "Habitat for Humanity",
-        "programUrl": "https://www.berea.edu/celts/community-service-programs/volunteer-opportunities/habitat-for-humanity-program/",
+        "programUrl": "https://www.instagram.com/bc_habitat/",
         "programDescription": "Through the work of Habitat for Humanity International, thousands of low-income families have found hope through affordable housing. Hard work and volunteering have resulted in the organization sheltering more than two million people worldwide.",
         "isStudentLed": False,
         "isBonnerScholars": False,
@@ -439,7 +439,7 @@ programs = [
     {
         "id": 7,
         "programName": "Berea Teen Mentoring",
-        "programUrl": "https://www.berea.edu/celts/community-service-programs/volunteer-opportunities/teen-mentoring-program/",
+        "programUrl": "https://www.instagram.com/bereateenmentoring/",
         "programDescription": "Berea Teen Mentoring (BTM) brings Berea community youth, from ages 13-18, into a group setting for mentorship and enrichment programs. Staff members are assisted during the weekly program by Berea College student volunteers, who act as mentors for these program participants. The mission of the program is to stimulate and cultivate personal growth for young adults in the Berea community.",
         "isStudentLed": True,
         "isBonnerScholars": False,
@@ -450,7 +450,7 @@ programs = [
     {
         "id": 8,
         "programName": "Hispanic Outreach Program",
-        "programUrl": "https://www.berea.edu/celts/community-service-programs/volunteer-opportunities/hispanic-outreach-project/",
+        "programUrl": "https://www.instagram.com/hop.bc_/",
         "programDescription": "The Hispanic Outreach Program (HOP) is a service-learning effort which brings together CELTS, several community organizations, and the Department of Foreign Languages at Berea College. HOP aims to build bridges among the Spanish-speaking and English-speaking residents of Madison County.",
         "isStudentLed": True,
         "isBonnerScholars": False,
@@ -472,7 +472,7 @@ programs = [
     {
         "id": 10,
         "programName": "Berea Tutoring",
-        "programUrl": "https://www.berea.edu/celts/community-service-programs/volunteer-opportunities/berea-tutoring-program/",
+        "programUrl": "https://www.instagram.com/bereatutoring/",
         "programDescription": "Berea Tutoring provides an encouraging atmosphere for local students who need help in achieving academic success, and for college volunteers who want to learn more about teaching or volunteering. Our mission is to increase conceptual understanding in academic subject areas, enrich educational experiences, and build self-confidence by providing college-aged tutors to local school children.",
         "isStudentLed": False,
         "isBonnerScholars": False,
