374ee7fe8625de3d8b9039b51639b5248bf01a15
Brian Ramsay <ramsayb2@berea.edu>
2023-10-19 13:41:46 -0400

    change other to a foreign key

--- app/models/requirementMatch.py
+++ app/models/requirementMatch.py
@@ -2,9 +2,10 @@ from app.models import *
 from app.models.certificationRequirement import CertificationRequirement
 from app.models.event import Event
 from app.models.course import Course
+from app.models.communityEngagementRequest import CommunityEngagementRequest
 
 class RequirementMatch(baseModel):
     requirement = ForeignKeyField(CertificationRequirement)
     event = ForeignKeyField(Event, null=True)
     course = ForeignKeyField(Course, null=True)
-    otherDescription = TextField(null=True)
+    engagementRequest = ForeignKeyField(CommunityEngagementRequest, null=True)
