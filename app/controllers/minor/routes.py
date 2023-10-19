f7a4c7642862559bb0113956f380f6dfed6a257a
Andersonstettner <stettnera30@gmail.com>
2023-10-19 10:01:58 -0400

    Clarified routes

--- app/controllers/minor/routes.py
+++ app/controllers/minor/routes.py
@@ -2,54 +2,45 @@ from flask import Flask, g
 
 from app.controllers.minor import minor_bp
 
-@minor_bp.route('/profile/<username>/cceMinor', methods=[''])
-def viewCceMinor():
+@minor_bp.route('/profile/<username>/cceMinor', methods=['GET'])
+def viewCceMinor(username):
+    """
+        Load minor management page with community engagements and summer experience
+    """
     pass
 
-@minor_bp.route('cceMinor/getTermActivities', methods=[''])
-def getTermActivities():
+@minor_bp.route('/cceMinor/<username>/identifyCommunityEngagement/<term>', methods=['GET'])
+def identifyCommunityEngagement(username):
+    """
+        Load all program and course participation records for that term
+    """
     pass
 
-@minor_bp.route('cceMinor/removeTermActivitie', methods=[''])
-def removeTermActivitie():
+@minor_bp.route('/cceMinor/<username>/addCommunityEngagement', methods=['POST'])
+def addCommunityEngagement(username):
+    """
+        Saving a term participation/activities for sustained community engagement 
+    """
     pass
 
-@minor_bp.route('/cceMinor/getCommunityEngagements', methods=[''])
-def getCommunityEngagements():
+@minor_bp.route('/cceMinor/<username>/removeCommunityEngagement', methods=['POST'])
+def removeCommunityEngagement(username):
+    """
+        Opposite of above
+    """
     pass
 
-@minor_bp.route('/cceMinor/getEventsInProgram', methods=[''])
-def getEventsInProgram():
+@minor_bp.route('/cceMinor/<username>/requestOtherCommunityEngagement', methods=['GET,POST'])
+def requestOtherCommunityEngagement(username):
+    """
+        Load the "request other" form and submit it.
+    """
     pass
 
-@minor_bp.route('/cceMinor/addCommunityEngagement', methods=[''])
-def addCommunityEngagement():
+@minor_bp.route('/cceMinor/<username>/addSummerExperience', methods=['POST'])
+def addSummerExperience(username):
     pass
 
-@minor_bp.route('/cceMinor/removeCommunityEngagement', methods=[''])
-def removeCommunityEngagement():
-    pass
-
-@minor_bp.route('/cceMinor/addOtherCommunityEngagement', methods=[''])
-def addOtherCommunityEngagement():
-    pass
-
-@minor_bp.route('/cceMinor/addSummerExperience', methods=[''])
-def addSummerExperience():
-    pass
-
-@minor_bp.route('/profile/<username>/cceMinor/otherCommunityEngagement', methods=[''])
-def requestOtherCommunityEngagement():
-    pass
-
-@minor_bp.route('/profile/<username>/cceMinor/addOtherCommunityEngagement', methods=[''])
-def addOtherCommunityEngagement():
-    pass
-
-@minor_bp.route('cceMinor/requestOtherEngagement/addSupervisorVerification', methods=[''])
-def addSupervisorVerification():
-    pass
-
-@minor_bp.route('/cceMinor/indicateInterest', methods=[''])
-def indicateMinorInterest():
+@minor_bp.route('/cceMinor/<username>/indicateInterest', methods=['POST'])
+def indicateMinorInterest(username):
     pass
