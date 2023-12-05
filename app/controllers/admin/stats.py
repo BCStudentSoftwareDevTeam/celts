f666ac19a4cf2215e0fc17793c85c49c8f8c7659
Chris <andersonc@berea.edu>
2023-12-05 10:54:41 -0500

    Removed term from spreadsheet html.

--- app/controllers/admin/stats.py
+++ app/controllers/admin/stats.py
@@ -10,17 +10,14 @@ def spreadsheetMaker():
     if not g.current_user.isCeltsAdmin:
         abort(403)
     
-    academicTerms = []
     academicYears = []
     allTerms = list(Term.select().order_by(Term.id))
     for term in allTerms:
-        academicTerms.append(term.description)
         if term.academicYear not in academicYears:
             academicYears.append(term.academicYear)
 
     return render_template("/admin/spreadsheetMaker.html",
-                           academicYears = academicYears,
-                           academicTerms = academicTerms)
+                           academicYears = academicYears)
 
 @admin_bp.route("/createSpreadsheet/", methods=["POST"])
 def createSpreadsheet():
