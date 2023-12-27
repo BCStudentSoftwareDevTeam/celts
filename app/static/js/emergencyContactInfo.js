bd9c80952a17af6a9866f3f918ff123fecbaf982
Daize Njounkeng <njounkengdaize@gmail.com>
2023-12-27 15:37:31 -0500

    fix lingering email validation error

--- app/static/js/emergencyContactInfo.js
+++ app/static/js/emergencyContactInfo.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
-
-    $('input.phone-input').inputmask('(999)-999-9999')
-
+    
     $('#emergencyContactEmail').on('input', function(){
-        if($(this).val() == '') {
-            this.setCustomValidity('')
+        let emailAddress = $(this).val();
+        if (emailAddress === '') {
+            this.setCustomValidity('');
+        } else if (!isValidEmailAddress(emailAddress)) {
+            this.setCustomValidity('Please enter a valid email address.');
+            this.reportValidity();
+        } else {
+            this.setCustomValidity('');
         }
     });
+
+    function isValidEmailAddress(emailAddress) {
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return emailRegex.test(emailAddress);
+    }
+
+    $('input.phone-input').inputmask('(999)-999-9999')
     $('input.phone-input').on('input', function(){
         let matches = $(this).val().match(/\d/g);
         let digits = matches?matches.length:0;
@@ -37,14 +48,12 @@ $(document).ready(function(){
     
     function formIsValid(){
         let invalidInputs = $('input').map(function(i, e){
-            if (! e.checkValidity()) {
-                if(e.id == "emergencyContactEmail") {
-                    this.setCustomValidity('Please enter a valid email address.')    
-                }
-                e.reportValidity()
-                return e
+            if (!e.checkValidity()) {
+                e.reportValidity();
+                return e;
             }
-        })
-        return invalidInputs.length == 0
+        });
+    
+        return invalidInputs.length == 0;
     }
 });
