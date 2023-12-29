7cbfec5d77d0cdc45a253deee744dc86ada07513
Mercy Eze <ezem@berea.edu>
2023-12-29 10:27:28 -0500

    Comment added

--- app/static/js/instructorTable.js
+++ app/static/js/instructorTable.js
@@ -10,20 +10,21 @@ $(document).ready(function() {
 
     $("#courseInstructor").on('input', function() {
         searchUser("courseInstructor", createNewRow, true, null, "instructor");
+        //sets autocomplete dropdown to top of screen stack
         setTimeout(function() {
             $(".ui-autocomplete").css("z-index", 9999);
         }, 500);
     });
     
-        // for each row in instructorTable that has an instructor, pass that instructors phone data to setupPhoneNumber
-    // $('#instructorTable tr').each(function(){
-    //     var username = getRowUsername(this)
-    //     var edit = "#editButton-" + username
-    //     var input = "#inputPhoneNumber-" + username
-    //     if (username){
-    //         setupPhoneNumber(edit, input)
-    //     }
-    // });
+        //for each row in instructorTable that has an instructor, pass that instructors phone data to setupPhoneNumber
+    $('#instructorTable tr').each(function(){
+        var username = getRowUsername(this)
+        var edit = "#editButton-" + username
+        var input = "#inputPhoneNumber-" + username
+        if (username){
+            setupPhoneNumber(edit, input)
+        }
+    });
 });
 
 function getRowUsername(element) {
