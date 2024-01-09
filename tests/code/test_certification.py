cef67f8515bdf62e2e0c96d1718066076786b7e0
Andersonstettner <stettnera30@gmail.com>
2024-01-09 11:47:26 -0500

    fixed broken test

--- tests/code/test_certification.py
+++ tests/code/test_certification.py
@@ -21,7 +21,7 @@ def test_getCertRequirements():
     cpr = allRequirements[3]['requirements']
     assert ["Volunteer Training", "CPR Training"] == [r.name for r in cpr]
 
-    cesc = getCertRequirements(certification=Certification.CESC)
+    cce = getCertRequirements(certification=Certification.CCE)
     bonner = getCertRequirements(certification=Certification.BONNER)
     assert len(bonner) == 9
 
