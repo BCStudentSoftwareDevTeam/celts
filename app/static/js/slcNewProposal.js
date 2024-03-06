a2f4339f21a9e1f16d434dbfe3ac8ce5c57f1b88
stevensonmichel <michels@berea.edu>
2024-03-06 14:34:31 -0500

    Make use of external functions for new proposals

--- app/static/js/slcNewProposal.js
+++ app/static/js/slcNewProposal.js
@@ -1,3 +1,4 @@
+import {getCourseInstructors, getRowUsername, createNewRow} from './instructorTable.js'
 import searchUser from './searchUser.js'
 
 var currentTab = 0; // Current tab is set to be the first tab (0)
@@ -120,7 +121,7 @@ $(document).ready(function(e) {
   // Add course instructor event handlers
   // -----------------------------------------
 
-      $("#instructorTable").on("click", ".removeButton", function() {
+      $("#instructorTable").on("click", "#remove", function() {
         let closestRow =  $(this).closest("tr");
         let username = closestRow.data('username');
 
@@ -310,58 +311,6 @@ function validateForm() {
 
   return valid;
 };
-
-// Instructor manipulation functions
-// -------------------------------------
-
-function getRowUsername(element) {
-    return $(element).closest("tr").data("username")
-}
-
-function createNewRow(selectedInstructor) {
-  let instructor = (selectedInstructor["firstName"]+" "+selectedInstructor["lastName"]+" ("+selectedInstructor["email"]+")");
-  let username = selectedInstructor["username"];
-  let phone = selectedInstructor["phoneNumber"];
-  let tableBody = $("#instructorTable").find("tbody");
-
-  if(tableBody.prop('outerHTML').includes(instructor)){
-    msgFlash("Instructor is already added.", "danger");
-    return;
-  }
-  // Create new table row and update necessary attributes
-  let lastRow = tableBody.find("tr:last");
-  let newRow = lastRow.clone();
-
-  let instructorName = newRow.find("td:eq(0) p")
-  instructorName.text(instructor);
-
-  let phoneInput = newRow.find("td:eq(0) input")
-  phoneInput.val(phone);
-  phoneInput.attr("id", "inputPhoneNumber-" +username);
-  $(phoneInput).inputmask('(999)-999-9999');
-
-  let removeButton = newRow.find("td:eq(1) button")
-  let editLink = newRow.find("td:eq(0) a")
-  editLink.attr("id", "editButton-" + username);
-
-  editLink.attr("data-username", username)
-  newRow.prop("hidden", false);
-  lastRow.after(newRow);
-
-  phoneInput.attr("data-value", phone)
-  var edit = "#editButton-" + username
-  var input = "#inputPhoneNumber-" + username
-  if (username){
-    setupPhoneNumber(edit, input)
-  }
-
-  $("#instructorTableNames").append('<input hidden name="instructor[]" value="' + username + '"/>')
-}
-
-function getCourseInstructors() {
-  // get usernames out of the table rows into an array
-  return $("#instructorTableNames input").map((i,el) => $(el).val())
-}
 
 function disableSyllabusUploadFile() {
   $("#fileUpload").prop("disabled", true);
