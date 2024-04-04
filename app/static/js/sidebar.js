a6369644ef3ca83847c86a5a61bf13a7ebd6e92e
bledsoef <bledsoef@berea.edu>
2024-04-04 16:14:38 -0400

    fixed the hover issue

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -1,23 +1,7 @@
 $(document).ready(function() {
-    // add hovers to describe what the numbers we are adding to the sidebar mean.
-    $(".courseManagement").popover({
-      trigger: "hover",
-      sanitize: false,
-      html: true,
-      content: function() {
-        return "Amount of pending course proposals for the current term."
-      }
-    });
-    $(".minorManagement").popover({
-      trigger: "hover",
-      sanitize: false,
-      html: true,
-      content: function() {
-          return "Amount of students who have expressed interest in the minor."
-      }
-    });
 
     // fetch the number of interested students and unapproved courses and display them in the sidebar if there are any
+    // and add hovers to describe what the numbers we are adding to the sidebar mean.
     $.ajax({
       url: "/admin/getSidebarInformation",
       type: "GET",
@@ -26,9 +10,25 @@ $(document).ready(function() {
         const interestedStudentsCount = Number(sidebarInformation.interestedStudentsCount)
         if (unapprovedCoursesCount > 0) {
           $("#courseManagement").html(`Course Management (${unapprovedCoursesCount})`)
+          $(".courseManagement").popover({
+            trigger: "hover",
+            sanitize: false,
+            html: true,
+            content: function() {
+              return "Amount of pending course proposals for the current term."
+            }
+          });
         } 
         if (interestedStudentsCount > 0) {
           $("#minorManagement").html(`Minor Management (${interestedStudentsCount})`)
+          $(".minorManagement").popover({
+            trigger: "hover",
+            sanitize: false,
+            html: true,
+            content: function() {
+                return "Amount of students who have expressed interest in the minor."
+            }
+          });
         }
         if (interestedStudentsCount + unapprovedCoursesCount > 0) {
           $("#admin").html(`Admin (${interestedStudentsCount + unapprovedCoursesCount})`)
