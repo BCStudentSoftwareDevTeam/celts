fa2467869c71086ebad843a6779491be68816983
Liliane Azabe <azabel@berea.edu>
2024-04-12 14:26:48 -0400

    added some additional information

--- app/static/js/cceMinorProfilePage.js
+++ app/static/js/cceMinorProfilePage.js
@@ -126,3 +126,18 @@ function toggleEngagementCredit(isChecked, engagementData, checkbox){
           }
     });
 }
+
+$(document).ready(function () {
+  $('.startDatePicker').datepicker({
+      format: 'mm-dd-yyyy',
+      autoclose: true 
+  });
+  $('.calendar-icon').on('click', function() {
+      var today = new Date();
+      var dd = String(today.getDate()).padStart(2, '0');
+      var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      var yyyy = today.getFullYear();
+      var formattedDate = mm + '-' + dd + '-' + yyyy;
+      $(this).closest('.input-group').find('.startDatePicker').val(formattedDate);
+  });
+});
