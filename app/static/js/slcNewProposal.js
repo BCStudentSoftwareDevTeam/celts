a3693824fbe997a234626f576fb4af8adcf354a3
Johncox2211 <johncox428@yahoo.com>
2024-06-18 16:37:32 +0000

    added some HTML in the button properties to test

--- app/static/js/slcNewProposal.js
+++ app/static/js/slcNewProposal.js
@@ -1,4 +1,5 @@
 import {getCourseInstructors, getRowUsername, createNewRow} from './instructorTable.js'
+
 import searchUser from './searchUser.js'
 
 var currentTab = 0; // Current tab is set to be the first tab (0)
