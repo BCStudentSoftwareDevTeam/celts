fb08c149e9414378fa996ca188c02de51afd3701
ndiayem <Ndiayem@berea.edu>
2024-06-18 15:58:25 +0000

    We fixed the problem where the 'unchecked' event file attachment is still displayed. We also enabled users to add more image types for event file attachments.

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,13 +1,30 @@
-from app import app
+# from app import app
+# import os
+# # from app.login_manager import require_login
+
+
+# @app.context_processor
+# def injectGlobalData():
+#     currentUser = require_login()
+#     lastStaticUpdate = str(max(os.path.getmtime(os.path.join(root_path, f))
+#                  for root_path, dirs, files in os.walk('app/static')
+#                   for f in files))
+#     return {'currentUser': currentUser,
+#            'lastStaticUpdate': lastStaticUpdate}
 import os
-#from app.login_manager import require_login
-
-
-#@app.context_processor
-#def injectGlobalData():
-    #currentUser = require_login()
-    #lastStaticUpdate = str(max(os.path.getmtime(os.path.join(root_path, f))
-    #              for root_path, dirs, files in os.walk('app/static')
-    #               for f in files))
-    #return {'currentUser': currentUser,
-    #        'lastStaticUpdate': lastStaticUpdate}
+import pprint
+from flask import Flask, render_template
+from playhouse.shortcuts import model_to_dict, dict_to_model
+from app.logic.config import load_config_files
+# Initialize our application
+app = Flask(__name__, template_folder="templates")
+app.env = os.environ.get('APP_ENV', 'production')
+load_config_files(app)
+print (" * Environment:", app.env)
+# set the secret key after configuration is set up
+app.secret_key = app.config['secret_key']
+# These imports must happen after configuration
+from app.models.term import Term
+from app.models.user import User
+from peewee import BaseQuery
+from flask import session
--- app/static/js/displayFilesMacro.js
+++ app/static/js/displayFilesMacro.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $("a.fileName").tooltip();
 
-    $(".removeAttachment").on("click", function(){
+    $(".removeAttachment").on("click", function() {
         let fileId = $(this).data("id");
         let deleteLink = $(this).data("delete-url");
         let fileData = {
@@ -13,32 +13,22 @@ $(document).ready(function(){
             type: "POST",
             url: deleteLink,
             data: fileData,
-            success: function(){
+            success: function() {
                 msgFlash("Attachment removed successfully", "success");
                 $("#attachment_" + fileId).remove();
             },
-            error: function(error){
+            error: function(error) {
                 msgFlash(error);
             }
         });
     });
 
     $('.attachmentCheck').change(function() {
-        // Store the current checkbox state
-        var isChecked = $(this).is(':checked');
-        
-        // Uncheck all checkboxes
-        $('.attachmentCheck').prop('checked', false);
-        
-        // Toggle the current checkbox
-        if (!isChecked) {
-            $(this).prop('checked', true);
-        } else {
-            $(this).prop('checked', false);
-        }
-
         var attachmentId = $(this).data('id');
         var isChecked = $(this).is(':checked');
+
+        // Uncheck all other checkboxes
+        $('.attachmentCheck').not(this).prop('checked', false);
 
         $.ajax({
             url: '/displayEventFile',
@@ -55,24 +45,4 @@ $(document).ready(function(){
             }
         });
     });
-
-    // Additional functionality to handle click events
-    document.querySelectorAll('.attachmentCheck').forEach(item => {
-        item.addEventListener('click', event => {
-            if (item.checked) {
-                item.checked = false; // This toggles the checkbox off if it was already checked
-            } else {
-                uncheckAll();
-                item.checked = true;
-                
-            }
-        });
-    });
-
-    function uncheckAll() {
-        document.querySelectorAll('.attachmentCheck').forEach(checkbox => {
-            checkbox.checked = false;
-        });
-    }
 });
-
