88f4ce067a87c43c7767033682e21b815ca595a4
zawn <zawn@berea.edu>
2024-06-26 09:02:54 -0400

    sidebars are editted but the exit is still not working

--- app/static/js/eventKiosk.js
+++ app/static/js/eventKiosk.js
@@ -1,14 +1,20 @@
 $(document).keydown(function(e){
-    if (e.key === "Escape") {
-        $("#fullscreenCheck").prop("checked", false)
-        toggleFullscreen();
-    }
-});
+  console.log(e.key)
+  if (e.key== "Escape"){
+    $("#fullscreenCheck").prop("checked", false)
+    console.log("here test")
+    hideElements(false);
+    document.exitFullscreen() || document.webkitExitFullscreen() || document.msExitFullscreen()
+    console.log("in toggle function keydown-2")
+  }
+})
 
 $(document).ready(function(e) {
     $("#submitScannerData").focus();
 
+
     $("#submitScannerData").keydown(function(e) {
+      console.log("in submit keydopnw")
         if (e.key === "Enter") {
             submitData();
         }
@@ -97,23 +103,29 @@ function hideElements(hide) {
     $("kiosk-hide").prop("disabled", true);
     $("position-fixed float-start").hide();
     $("position-fixed float-start").css("width", "0");
+    $("#sideBarContainer").attr("class", "col-md-0  d-print-none d-none d-lg-block")
+    $("#sideBar").attr("class", "position-fixed float-start d-none")
+    $("#sideBarSandwichUI").attr("class","btn btn-dark rounded-0 rounded-end kiosk-hide d-none")
   } else 
    {
+    console.log("in hide element before keydown")
     $("footer").show();
     $("kiosk-hide").css("width", "inherit");
     $("kiosk-hide").animate({ opacity: 1 }, 1);
     $("kiosk-hide").prop("disabled", false);
     $("a").show();
-   
+    $("#sideBarContainer").attr("class", "col-md-auto d-print-none d-none d-lg-block")
+    $("#sideBar").attr("class", "position-fixed float-start")
+    $("#sideBarSandwichUI").attr("class","btn btn-dark rounded-0 rounded-end kiosk-hide")
+    console.log("in hide element after keydown")
   }
 }
 
 // Source: https://stackoverflow.com/questions/1125084/how-to-make-the-window-full-screen-with-javascript-stretching-all-over-the-screen
 function toggleFullscreen() {
-  if($("#fullscreenCheck").prop("checked") == false){
-    hideElements(false);
-    document.exitFullscreen() || document.webkitExitFullscreen() || document.msExitFullscreen()
-  } else {
+  if($("#fullscreenCheck").prop("checked") == true){
+    exited = true
+    console.log(exited)
     hideElements(true);
 
     var el = document.documentElement
@@ -123,16 +135,27 @@ function toggleFullscreen() {
     || el.mozRequestFullScreen
     || el.msRequestFullscreen
     ;
+    
     if(typeof rfs!="undefined" && rfs){
       rfs.call(el);
-    } else if(typeof window.ActiveXObject!="undefined"){
+  }else if(typeof window.ActiveXObject!="undefined"){
       // for Internet Explorer
       var wscript = new ActiveXObject("WScript.Shell");
       if (wscript!=null) {
         wscript.SendKeys("{F11}");
       }
-    }
+    } 
+  }else if ($("#fullscreenCheck").prop("checked") == false){
+    console.log("here test")
+    hideElements(false);
+    document.exitFullscreen() || document.webkitExitFullscreen() || document.msExitFullscreen()
+    console.log("in toggle function keydown-2")
   }
   $('#submitScannerData').focus();
 };
-
+/*if (!document.fullscreenElement && !document.webkitFullscreenElement && !document.mozFullScreenElement && !document.msFullscreenElement) {
+  console.log("Fullscreen mode was exited");
+}*/
+if (document.fullscreenElement && document.webkitFullscreenElement && document.mozFullScreenElement && document.msFullscreenElement) {
+  console.log("Fullscreen mode was entered");
+}
