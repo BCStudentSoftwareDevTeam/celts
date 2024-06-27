17665326315e6712004b7ae9dfe90c811ef7aa21
zawn <zawn@berea.edu>
2024-06-27 21:04:59 +0000

    I have done research on event listner

--- app/static/js/eventKiosk.js
+++ app/static/js/eventKiosk.js
@@ -1,4 +1,12 @@
 var elem = document.getElementById("show");
+
+$(document).on("fullscreenchange", function(){
+  if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
+    closeFullscreen();
+  } else {
+    openFullscreen();
+  }
+})
 $(document).keydown(function(e) {
   if (e.key === "F11") {
     e.preventDefault();
@@ -134,3 +142,77 @@ function submitData(){
       }
     })
 }
+
+/*function hideElements(hide) {
+  if (hide == true) {
+    $("col-md-auto d-print-none d-lg-none").css("width", "0");
+    $("footer").hide();
+    $("kiosk-hide").animate({ opacity: 0 }, 1);
+    $("kiosk-hide").css("width", "0");
+    $("kiosk-hide").prop("disabled", true);
+    $("position-fixed float-start").hide();
+    $("position-fixed float-start").css("width", "0");
+    $("#sideBarContainer").attr("class", "col-md-0  d-print-none d-none d-lg-block")
+    $("#sideBar").attr("class", "position-fixed float-start d-none")
+    $("#sideBarSandwichUI").attr("class","btn btn-dark rounded-0 rounded-end kiosk-hide d-none")
+  } else 
+   {
+    console.log("in hide element before keydown")
+    $("footer").show();
+    $("kiosk-hide").css("width", "inherit");
+    $("kiosk-hide").animate({ opacity: 1 }, 1);
+    $("kiosk-hide").prop("disabled", false);
+    $("a").show();
+    $("#sideBarContainer").attr("class", "col-md-auto d-print-none d-none d-lg-block")
+    $("#sideBar").attr("class", "position-fixed float-start")
+    $("#sideBarSandwichUI").attr("class","btn btn-dark rounded-0 rounded-end kiosk-hide")
+    console.log("in hide element after keydown")
+  }
+}
+
+// Source: https://stackoverflow.com/questions/1125084/how-to-make-the-window-full-screen-with-javascript-stretching-all-over-the-screen
+function toggleFullscreen() {
+  if($("#fullscreenCheck").prop("checked") == true){
+    hideElements(true);
+    var el = document.documentElement
+    , rfs = // for newer Webkit and Firefox
+    el.requestFullscreen
+    || el.webkitRequestFullScreen
+    || el.mozRequestFullScreen
+    || el.msRequestFullscreen
+    ;
+    
+    if(typeof rfs!="undefined" && rfs){
+      rfs.call(el);
+      exited = false
+
+    }else if(typeof window.ActiveXObject!="undefined"){
+      // for Internet Explorer
+      exited = false
+
+      var wscript = new ActiveXObject("WScript.Shell");
+      if (wscript!=null) {
+        exited = false
+
+        wscript.SendKeys("{F11}");
+      }
+    } 
+  }else if ($("#fullscreenCheck").prop("checked") == false){
+    hideElements(false);
+    document.exitFullscreen() || document.webkitExitFullscreen() || document.msExitFullscreen()
+  }
+  $('#submitScannerData').focus();
+
+};
+/*  if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
+  console.log("Fullscreen mode was entered");
+  hideElements(true);
+  console.log("here wewe")
+} else {
+  console.log("Fullscreen mode was exited");
+}if (!document.fullscreenElement && !document.webkitFullscreenElement && !document.mozFullScreenElement && !document.msFullscreenElement) {
+  console.log("Fullscreen mode was exited");
+}*/
+
+  
+
