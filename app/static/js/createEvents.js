5e6e54644080bd4255b637ea849d7593917265b4
Anupriya Dixit <dixita@berea.edu>
2024-07-02 22:49:09 +0000

    json error fixed

--- app/static/js/createEvents.js
+++ app/static/js/createEvents.js
@@ -357,9 +357,9 @@ $(document).ready(function() {
     }
   });
 
-  $(".customDate").click(function() {
-    $("#customDatePicker-" + $(this).data("page-location")).datepicker().datepicker("show");
-  });
+  // $(".customDate").click(function() {
+  //   $("#customDatePicker-" + $(this).data("page-location")).datepicker().datepicker("show");
+  // });
 
   $("#checkRSVP").click(function(){
     if ($("input[name='isRsvpRequired']:checked").val() == 'on'){
@@ -411,9 +411,9 @@ $(document).ready(function() {
      updateDate(this)
   });
 
-  $(".customDatePicker").change(function(){ //custom data calender function
-    updateDate(this)
- });
+//   $(".customDatePicker").change(function(){ //custom data calender function
+//     updateDate(this)
+//  });
 
   $("#inputCharacters").keyup(function(event){
     setCharacterLimit(this, "#remainingCharacters")
