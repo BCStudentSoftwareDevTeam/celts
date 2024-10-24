a6e3aee1f93676aa3a919ff9a1e4f462c7420a74
James <makindeo@berea.edu>
2024-10-24 16:17:21 -0400

    Updated SQL script

--- database/oneOff/merge-series-table.sql
+++ database/oneOff/merge-series-table.sql
@@ -10,30 +10,6 @@ begin
     update event set seriesId=recurringId where recurringId is not null;
     set seriesIdvalue = coalesce((select max(seriesId) from event), 0) + 1;
 
-    INSERT INTO event (contactEmail, contactName, deletedBy, 
-    deletionDate, description, endDate, 
-    id, isAllVolunteerTraining, isCanceled, 
-    isFoodProvided, isRsvpRequired, isService, isTraining, 
-    location, multipleOfferingId, name, program_id, rsvpLimit, startDate, term_id, timeEnd, 
-    timeStart) 
-    VALUES 
-    ('contact@example.com', 'James', '5', 
-    '2024-10-29', 'New script test.', '2024-10-29', 989, FALSE, FALSE, TRUE, TRUE, FALSE, TRUE, '123 Event Location', 3, 
-    'Test Event', 1, 100, '2024-10-22', 19, '18:00:00', '10:00:00'), 
-    ('contact@example.com', 'James', '5', 
-    '2024-10-29', 'New script test.', '2024-10-29', 67676, FALSE, FALSE, TRUE, TRUE, FALSE, TRUE, '123 Event Location', 3, 
-    'Test Event', 1, 100, '2024-10-22', 19, '18:00:00', '10:00:00'), 
-    ('contact@example.com', 'Anna', '5', 
-    '2024-10-29', 'New script test.', '2024-10-29', 45454545, FALSE, FALSE, TRUE, TRUE, FALSE, TRUE, '123 Event Location', 4, 
-    'Test Event', 1, 100, '2024-10-22', 19, '18:00:00', '10:00:00'), 
-    ('contact@example.com', 'Anna', '5', 
-    '2024-10-29', 'New script test.', '2024-10-29', 846756, FALSE, FALSE, TRUE, TRUE, FALSE, TRUE, '123 Event Location', 4, 
-    'Test Event', 1, 100, '2024-10-22', 19, '18:00:00', '10:00:00'),
-    ('contact@example.com', 'Brian', '5', 
-    '2024-10-29', 'New script test.', '2024-10-29', 9090909, FALSE, FALSE, TRUE, TRUE, FALSE, TRUE, '123 Event Location', 5, 
-    'Test Event', 1, 100, '2024-10-22', 19, '18:00:00', '10:00:00');
-
-
     update event set seriesId=recurringId, isRepeating=1 where recurringId is not null;
     open cur1;
 
