d1081b053824ea72bb477e39c702f2ec0a4d00b6
zawn <zawn@berea.edu>
2025-01-22 17:48:15 -0500

    have edited parts of the base.html of the front-end so that the toast message will be in the center when there's no undo button but it will be space around when there is an undo button, added a test function for deleteUserbackgroundCheck

--- tests/code/test_users.py
+++ tests/code/test_users.py
@@ -3,7 +3,7 @@ from peewee import *
 from datetime import datetime
 from dateutil import parser
 from app import app
-from flask import g
+from flask import g, request, session
 
 from app.models import mainDB
 from app.models.program import Program
@@ -15,7 +15,52 @@ from app.models.programManager import ProgramManager
 from app.models.backgroundCheck import BackgroundCheck
 from app.models.event import Event
 from app.logic.users import addUserInterest, removeUserInterest, banUser, unbanUser, isEligibleForProgram, getUserBGCheckHistory, addProfileNote, deleteProfileNote, getBannedUsers, isBannedFromEvent, updateDietInfo
-from app.logic.volunteers import addUserBackgroundCheck
+from app.logic.volunteers import addUserBackgroundCheck, deleteUserBackgroundCheck
+
+@pytest.mark.integration
+def test_deleteUserBackgroundCheck():
+    with mainDB.atomic() as transaction:
+        with app.app_context():
+            g.current_user = "ramsayb2"
+
+            # Create a test user to run background checks on
+            testusr = User.create(username = 'zawn',
+                                    firstName = 'Nyan',
+                                    lastName = 'Zaw',
+                                    bnumber = '00123321',
+                                    email = 'zawn@berea.deu',
+                                    isStudent = True)
+
+            # Add background checks to the user
+            addUserBackgroundCheck("zawn","CAN", "Submitted", parser.parse("2022-10-21"))
+            addUserBackgroundCheck("zawn","SHS", "Submitted", parser.parse("2023-11-18"))
+            testusrHistory = getUserBGCheckHistory(testusr)
+            print("here")
+
+            # Check that all the users background checks have been submitted and
+            # they are returned correctly. Also make sure that the background check
+            # that has not been given anything is returend as empty
+            assert "Submitted" == testusrHistory['CAN'][0].backgroundCheckStatus
+            assert "Submitted" == testusrHistory['SHS'][0].backgroundCheckStatus
+            assert [] == testusrHistory['FBI']
+            assert [] == testusrHistory['BSL']
+            
+            # Delete the submitted background checks and make sure the deleted background check 
+            # are no longer present in the CAN and SHS dictionaries
+            CAN_bgcheck_first = testusrHistory['CAN'][0]
+            deleteUserBackgroundCheck(CAN_bgcheck_first.id, "zawn")
+            SHS_bgcheck_first = testusrHistory['SHS'][0]
+            deleteUserBackgroundCheck(SHS_bgcheck_first.id, "zawn")
+            testusrHistory = getUserBGCheckHistory(testusr)
+            
+            #Check that all the users background checks have been deleted
+            assert [] == testusrHistory['CAN']
+            assert [] == testusrHistory['SHS']
+            assert [] == testusrHistory['FBI']
+            assert [] == testusrHistory['BSL']
+   
+
+        transaction.rollback()
 
 @pytest.mark.integration
 def test_user_model():
