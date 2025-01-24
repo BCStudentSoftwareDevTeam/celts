b8f88faf62ab8856096a142259f609dac9a8b2a2
bledsoef <bledsoef@berea.edu>
2025-01-24 14:30:57 -0500

    deleted unused javascript

--- app/static/js/minorProfilePage.js
+++ app/static/js/minorProfilePage.js
@@ -1,22 +1,6 @@
-import searchUser from './searchUser.js';
-
-function callback(selected) {
-  $("#searchStudentsInput").val(`${selected.firstName} ${selected.lastName}`);
-  $("#studentEmail").val(selected.email);
-}
-
 $(document).ready(function() {
   $('#hoursBelow300Container').hide()
   $('#otherExperienceDescription').hide()
-  $("#searchStudentsInput").on("input", function() {
-    searchUser("searchStudentsInput", callback);
-  });
-
-  $("#searchIcon").click(function (e) {
-    e.preventDefault();
-    let selected = { firstName: $("#searchStudentsInput").val().split(" ")[0], lastName: $("#searchStudentsInput").val().split(" ")[1] };
-    callback(selected);
-  });
 
   $("input[name='experienceType']").on("change", function() {
     toggleOtherExperienceTextarea();
@@ -33,7 +17,6 @@ $(document).ready(function() {
       textareaContainer.show()
     }
   }
-  window.toggleTextarea = toggleTextarea; 
 
   function toggleOtherExperienceTextarea() {
     var otherRadio = $('#otherExperience');
@@ -44,35 +27,6 @@ $(document).ready(function() {
       textareaContainer.style.display = 'none';
     }
   }
-  window.toggleOtherExperienceTextarea = toggleOtherExperienceTextarea;
-
-  function updateYearOptions() {
-    var submissionDate = new Date($('#date').value);
-    var currentYear = submissionDate.getFullYear();
-    var month = submissionDate.getMonth() + 1;
-    var day = submissionDate.getDate();
-    var defaultYear = (month < 5 || (month === 5 && day < 16)) ? currentYear : currentYear + 1;
-
-    var startYear = currentYear - 3;
-    var endYear = currentYear + 2;
-
-    var options = [];
-    for (var year = startYear; year <= endYear; year++) {
-      var option = $("<option>").val(year).text(year);
-      if (year === defaultYear) {
-        option.attr("selected", "selected");
-      }
-      options.push(option);
-    }
-
-    $("#summerYear").empty().append(options);
-  }
-
-  var today = new Date().toISOString().split('T')[0];
-  $('#date').value = today;
-  updateYearOptions();
-
-  $('#date').addEventListener('change', updateYearOptions);
 
   $('#summerExperienceForm').on('submit', function(event) {
     event.preventDefault(); 
@@ -94,25 +48,4 @@ $(document).ready(function() {
       }
     });
   });
- $('#edit-proposal-button').on('click', function() {
-    toggleEditMode(true);
-  });
-
-  $('#cancel-edit-button').on('click', function() {
-    toggleEditMode(false);
-  });
-
-  function toggleEditMode(isEditMode) {
-    document.querySelectorAll('input, textarea, select').forEach(element => {
-      if (element.type === 'radio' || element.type === 'checkbox' || element.tagName === 'SELECT') {
-        element.disabled = !isEditMode;
-      } else {
-        element.readOnly = !isEditMode;
-      }
-    });
-    $('#edit-buttons').style.display = isEditMode ? 'block' : 'none';
-    $('#view-buttons').style.display = isEditMode ? 'none' : 'block';
-  }
-
-  toggleEditMode(false);
 })
