feaf4e365e104d9a387c1bc3f57e23bb31e592aa
zawn <zawn@berea.edu>
2025-01-29 14:38:08 -0500

    branch created

--- app/controllers/main/routes.py
+++ app/controllers/main/routes.py
@@ -79,6 +79,9 @@ def events(selectedTerm, activeTab, programID):
     currentTime = datetime.datetime.now()
 
     listOfTerms = Term.select()
+    term_Order= [(term.termOrder, term.description, term.id) for term in listOfTerms]
+    print("imran", term_Order)
+
     participantRSVP = EventRsvp.select(EventRsvp, Event).join(Event).where(EventRsvp.user == g.current_user)
     rsvpedEventsID = [event.event.id for event in participantRSVP]
 
