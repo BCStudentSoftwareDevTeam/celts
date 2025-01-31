80810de9219b6d956c857f21c730b1527dc55580
bledsoef <bledsoef@berea.edu>
2025-01-31 14:41:49 -0500

    focused search on page load

--- app/static/js/searchStudent.js
+++ app/static/js/searchStudent.js
@@ -2,13 +2,15 @@ import searchUser from './searchUser.js'
 function callback(selected) {
   $("#searchStudent").submit();
 }
-
-$("#searchStudentsInput").on("input", function() {
-  searchUser("searchStudentsInput", callback);
-});
-
-$("#searchIcon").click(function (e) {
-  e.preventDefault();
-  callback($("#searchStudentsInput").val());
-});
+$(document).ready(function() {
+  $("#searchStudentsInput").on("input", function() {
+    searchUser("searchStudentsInput", callback);
+  });
+  
+  $("#searchIcon").click(function (e) {
+    e.preventDefault();
+    callback($("#searchStudentsInput").val());
+  });
+  $("#searchStudentsInput").focus() 
+})
 
