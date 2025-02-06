170ebfdb5436d8dde36934a60424f4c780f1729b
bledsoef <bledsoef@berea.edu>
2025-02-06 16:59:20 -0500

    updated models to be similar to one another

--- app/models/otherExperience.py
+++ app/models/otherExperience.py
@@ -1,14 +1,22 @@
 from app.models import *
 from app.models.term import Term
 from app.models.user import User
+import datetime
 
 class OtherExperience(baseModel):
     student = ForeignKeyField(User)
-    activity = CharField()
     term = ForeignKeyField(Term)
     hours = IntegerField()
     weeks = IntegerField()
-    service = CharField()
-    company = CharField()
-    createdBy = ForeignKeyField(User)
+    experienceTitle = CharField()
+    experienceDescription = CharField()
+    status = CharField(constraints=[Check("status in ('Approved', 'Pending', 'Denied')")], default='Pending')
+    orgName= CharField()
+    orgAddress = CharField()
+    orgPhone = CharField()
+    orgWebsite = CharField()
     supervisorName = CharField()
+    supervisorPhone = CharField()
+    supervisorEmail = CharField()
+    createdOn = DateTimeField(default=datetime.datetime.now)
+    createdBy = ForeignKeyField(User)
