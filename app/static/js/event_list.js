4cecd3d07c330cf7bef99a8dc2d280f9d7deb4ba
WackyWeaver <lintelmannaders@berea.edu>
2025-02-07 15:56:00 -0500

    made broadstrokes changes that replicate common functionality for engagement event list

--- app/static/js/event_list.js
+++ app/static/js/event_list.js
@@ -79,6 +79,7 @@ function updateIndicatorCounts(isChecked){
     success: function(eventsCount) {
       const studentLedEventsCount = Number(eventsCount.studentLedEventsCount);
       const trainingEventsCount = Number(eventsCount.trainingEventsCount);
+      const engagementEventsCount = Number(eventsCount.engagementEventsCount)
       const bonnerEventsCount = Number(eventsCount.bonnerEventsCount);
       const otherEventsCount = Number(eventsCount.otherEventsCount);
       const toggleStatus = eventsCount.toggleStatus;
@@ -87,7 +88,8 @@ function updateIndicatorCounts(isChecked){
 
       // use ternary operators to populate the tab with a number if there are events, and clear the count if there are none
       studentLedEventsCount > 0 ? $("#studentLedEvents").html(`Student Led Service (${studentLedEventsCount})`) : $("#studentLedEvents").html(`Student Led Service`)
-      trainingEventsCount > 0 ? $("#trainingEvents").html(`Training and Education (${trainingEventsCount})`) : $("#trainingEvents").html(`Training and Education`)
+      trainingEventsCount > 0 ? $("#trainingEvents").html(`Training (${trainingEventsCount})`) : $("#trainingEvents").html(`Training and Education`)
+      engagementEventCount > 0 ? $("#engagementEvents").html(`Education and Engagement this(${engagementEventsCount})`) : $("#engagementEvents").html('Education and Engagement')
       bonnerEventsCount > 0 ? $("#bonnerScholarsEvents").html(`Bonner Scholars (${bonnerEventsCount})`) : $("#bonnerScholarsEvents").html(`Bonner Scholars`)
       otherEventsCount > 0 ? $("#otherEvents").html(`Other Events (${otherEventsCount})`) : $("#otherEvents").html(`Other Events`)
     },
