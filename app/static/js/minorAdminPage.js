9021964fe7e24732f394a9a0f1bea1a72404d6d6
Arohasina <ravoahanginiainaa@berea.edu>
2025-07-08 14:56:46 -0400

    got rid of a space

--- app/static/js/minorAdminPage.js
+++ app/static/js/minorAdminPage.js
@@ -124,7 +124,6 @@ function updateInterestedStudents(){
     $("#addInterestedStudentsbtn").prop("disabled", true)
   } else {
     $("#addInterestedStudentsbtn").prop("disabled", false)
-   
     msgFlash("Succssesfully added student intrested in minor.", "success", 1300, true)
   }
 }
