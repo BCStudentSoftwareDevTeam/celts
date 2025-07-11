8efb32374b2b8b8a2e054089a38018c24ce0762b
mathew2005 <mathewoswondwosen2500@gmail.com>
2025-07-11 16:26:03 -0400

    Added global data so sidebar can acess Program Manager data. Still needs to be turned into meaningful data to serve as a check for the Create Event List in the sidebar

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,8 +1,9 @@
 from app import app
 import os
 #from app.login_manager import require_login
-
-
+from  app.models.programManager import ProgramManager
+from  app.models.user import User
+from flask import g
 #@app.context_processor
 #def injectGlobalData():
     #currentUser = require_login()
@@ -11,3 +12,20 @@ import os
     #               for f in files))
     #return {'currentUser': currentUser,
     #        'lastStaticUpdate': lastStaticUpdate}
+
+@app.context_processor
+def injectGlobalData():
+    return {
+        "programManager" : ProgramManager
+                         .select(ProgramManager, User)
+                         .join(User)
+                         .where(User.username == g.current_user)
+                         .execute(),
+        "currentUser": g.current_user
+    }
+
+
+#   user = (User.select(User, EmergencyContact, InsuranceInfo)
+#                 .join(EmergencyContact, JOIN.LEFT_OUTER).switch()
+#                 .join(InsuranceInfo, JOIN.LEFT_OUTER)
+#                 .where(User.username == username).limit(1))
