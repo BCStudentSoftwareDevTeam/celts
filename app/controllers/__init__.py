18c1967871188859629c96669d479b8ea7d20f91
mathew2005 <mathewoswondwosen2500@gmail.com>
2025-07-15 14:29:22 -0400

    removed no longer used code to inject global data

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,9 +1,7 @@
 from app import app
 import os
 #from app.login_manager import require_login
-from  app.models.programManager import ProgramManager
-from  app.models.user import User
-from flask import g
+
 #@app.context_processor
 #def injectGlobalData():
     #currentUser = require_login()
@@ -12,8 +10,3 @@ from flask import g
     #               for f in files))
     #return {'currentUser': currentUser,
     #        'lastStaticUpdate': lastStaticUpdate}
-
-@app.context_processor
-def injectGlobalData():
-    isProgramManager = True if list(ProgramManager.select(ProgramManager, User).join(User).where(User.username == g.current_user).execute()) else False 
-    return {"isProgramManager" : isProgramManager}
