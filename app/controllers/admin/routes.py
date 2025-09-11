5f49372dc9863378098af2ea2159205d4053a1f6
Oluwagbayi James Makinde <122561188+ojmakinde@users.noreply.github.com>
2025-09-11 14:20:16 -0400

    Removed the ability for student staff to create events in all programs

--- app/controllers/admin/routes.py
+++ app/controllers/admin/routes.py
@@ -82,7 +82,7 @@ def templateSelect():
 
 @admin_bp.route('/eventTemplates/<templateid>/<programid>/create', methods=['GET','POST'])
 def createEvent(templateid, programid):
-    if not (g.current_user.isAdmin or g.current_user.isProgramManagerFor(programid)):
+    if not (g.current_user.isCeltsAdmin or g.current_user.isProgramManagerFor(programid)):
         abort(403)
 
     # Validate given URL
