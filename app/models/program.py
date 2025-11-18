0e1cb1c61e649cf16e12c3b3864e5a1c82dfdb66
bakobagassas <bakobagassas@berea.edu>
2025-11-18 14:46:45 -0500

    Volunteer opp. in programs

--- app/models/program.py
+++ app/models/program.py
@@ -9,7 +9,6 @@ class Program(baseModel):
     bereaUrl = TextField(null=True)
     programDescription = TextField()
     partner = CharField(null=True)
-    isVolunteerOpportunities = BooleanField(default=False)
     isBonnerScholars = BooleanField(default=False)
     isOtherCeltsSponsored = BooleanField(default=False)
     contactName = CharField(null=True,default='')
