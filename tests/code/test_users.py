d974b894bccb2864363c120274e4a3b49bc34a8a
bakobagassas <bakobagassas@berea.edu>
2026-01-23 13:13:51 -0500

    New test for isCurrentlyEnrolled function

--- tests/code/test_users.py
+++ tests/code/test_users.py
@@ -492,3 +492,49 @@ def test_hasCurrentCeltsLabor():
         # Check if the new user has current Celts Labor
         assert newUser.hasCurrentCeltsLabor is False
         transaction.rollback()
+ 
+    
+@pytest.mark.integration
+def test_isCurrentlyEnrolled():
+    #testing the isCurrentlyEnrolled property of User
+    with mainDB.atomic() as transaction:
+        # Currently enrolled student
+        enrolledUser = User.create(
+            username="enrolleduser",
+            firstName="Enrolled",
+            lastName="Student",
+            bnumber="B10000004",
+            email="enrolled@berea.edu",
+            isStudent=True,
+            hasGraduated=False,
+            rawClassLevel="Junior"
+        )
+        assert enrolledUser.isCurrentlyEnrolled is True
+
+        # Alumni (graduated)
+        alumniUser = User.create(
+            username="alumniuser",
+            firstName="Alumni",
+            lastName="User",
+            bnumber="B10000005",
+            email="alumni@berea.edu",
+            isStudent=False,
+            hasGraduated=True,
+            rawClassLevel="Graduated"
+        )
+        assert alumniUser.isCurrentlyEnrolled is False
+
+        # Fall graduate (Graduating → Alumni)
+        fallGradUser = User.create(
+            username="fallgraduser",
+            firstName="Fall",
+            lastName="Grad",
+            bnumber="B10000006",
+            email="fall@berea.edu",
+            isStudent=False,
+            hasGraduated=False,
+            rawClassLevel="Graduating"
+        )
+        assert fallGradUser.isCurrentlyEnrolled is False
+
+        transaction.rollback()
