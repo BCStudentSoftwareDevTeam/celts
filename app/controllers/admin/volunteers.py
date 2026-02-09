fb6af41eb0012cfdbbf56fb47476632c1d1d5de0
Ariana Meatchem <146577403+Meatchema@users.noreply.github.com>
2026-02-09 15:20:23 -0500

    Removed datetime restriction so that scanned user can show in volunteer details

--- app/controllers/admin/volunteers.py
+++ app/controllers/admin/volunteers.py
@@ -115,7 +115,7 @@ def volunteerDetailsPage(eventID):
     
     waitlistUser = list(set([obj for obj in eventRsvpData if obj.rsvpWaitlist]))
     rsvpUser = list(set([obj for obj in eventRsvpData if not obj.rsvpWaitlist ]))
-    attendedUser = list(set([obj for obj in eventParticipantData if not obj.rsvpWaitlist and datetime.now() >= datetime.combine(event.startDate, event.timeStart)]))
+    attendedUser = list(set([obj for obj in eventParticipantData if not obj.rsvpWaitlist]))
     
     return render_template("/events/volunteerDetails.html",
                             waitlistUser = waitlistUser,
