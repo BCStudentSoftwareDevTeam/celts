d1ef839ac7a92b98a7684b78967a78c2cb8c4449
Ariana Meatchem <146577403+Meatchema@users.noreply.github.com>
2026-03-23 16:34:20 -0400

    Code reversion except grad changes, trying to figure out why card display and table isnt functioning completely.

--- app/controllers/admin/volunteers.py
+++ app/controllers/admin/volunteers.py
@@ -115,7 +115,6 @@ def volunteerDetailsPage(eventID):
     
     waitlistUser = list(set([obj for obj in eventRsvpData if obj.rsvpWaitlist]))
     rsvpUser = list(set([obj for obj in eventRsvpData if not obj.rsvpWaitlist ]))
-
     
     return render_template("/events/volunteerDetails.html",
                             waitlistUser = waitlistUser,
