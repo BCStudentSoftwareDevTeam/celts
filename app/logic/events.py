2d35b9e63cb430415b0351db5559807ad9123b47
bakobagassas <bakobagassas@berea.edu>
2026-03-24 15:14:24 -0400

    made the date format more consistent.

--- app/logic/events.py
+++ app/logic/events.py
@@ -120,7 +120,7 @@ def attemptSaveMultipleOfferings(eventData, attachmentFiles = None):
 
     # Create separate event data for each event in the series, inheriting from the original eventData
 
-    seriesData = sorted(eventData.get('seriesData'), key=lambda x: datetime.strptime(f"{x['eventDate']} {x['startTime']}",'%m/%d/%Y %H:%M'))
+    seriesData = sorted(eventData.get('seriesData'), key=lambda x: datetime.strptime(f"{x['eventDate']} {x['startTime']}",'%m/%d/%Y %H:%M')) # sorts the events in the series by date and time so that the events are created in order and the naming convention of Week 1, Week 2, etc. is consistent with the order of the events.
     isRepeating = bool(eventData.get('isRepeating'))
     with mainDB.atomic() as transaction:
         for index, event in enumerate(seriesData):
