f0e454790b938fbedd16d3c1319febac58322bf9
JohnCox2211 <143194918+JohnCox2211@users.noreply.github.com>
2026-04-15 16:30:36 -0400

    Update graduationManagement.js

--- app/static/js/graduationManagement.js
+++ app/static/js/graduationManagement.js
@@ -122,7 +122,7 @@ $(document).ready(function() {
     }
     
     function redrawTable() {
-        gradStudentsTable.draw();  
+        gradStudentsTable.draw();
     }
 
     function checkboxClickHandler() {
