08eea6efa6ed34a26bdc6f7e14c902562c82ba08
Scott Heggen <heggens@berea.edu>
2026-07-29 16:35:43 -0400

    restoring run_tests.sh to less verbose output"

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,7 +4,7 @@ fi
 
 APP_ENV="testing"
 
-FLAGS="$1 --capture=no --disable-pytest-warnings --strict-markers  -rs --ignore=tests/mail_test.py --ignore=db_test.py --ignore=database --ignore=run.py --ignore=app --ignore=venv"
+FLAGS="$1 --capture=no --disable-pytest-warnings --strict-markers --tb=line -rs --ignore=tests/mail_test.py --ignore=db_test.py --ignore=database --ignore=run.py --ignore=app --ignore=venv"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
