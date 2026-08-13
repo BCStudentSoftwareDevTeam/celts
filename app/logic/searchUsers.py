bbddd858397431e15596574252e7f5e8510e7b45
zawn <zawn@berea.edu>
2026-08-13 14:56:15 -0400

    search query click fix, search query prefix, suffix fixed, report download fix, sidebar enhanced, new config line added

--- app/logic/searchUsers.py
+++ app/logic/searchUsers.py
@@ -1,3 +1,4 @@
+from peewee import fn
 from playhouse.shortcuts import model_to_dict
 from app.models.user import User
 def searchUsers(query, category=None):
@@ -8,12 +9,28 @@ def searchUsers(query, category=None):
     '''
     # add wildcards to each piece of the query
     splitSearch = query.strip().split()
+    if not splitSearch:
+        return User.select().where(False)
     fullSearch = " ".join(splitSearch) + "%"
     searchWhere = (User.firstName ** fullSearch | User.lastName ** fullSearch | User.username ** fullSearch)
     for splitIndex in range(1, len(splitSearch)):
         firstName = " ".join(splitSearch[:splitIndex]) + "%"
         lastName = " ".join(splitSearch[splitIndex:]) + "%"
-        searchWhere |= (User.firstName ** firstName & User.lastName ** lastName)
+
+        searchWhere |= (
+            (User.firstName ** firstName) &
+            (User.lastName ** lastName)
+        )
+
+    # Also allow individual pieces of the name to match
+    for namePart in splitSearch:
+        nameSearch = namePart + "%"
+
+        searchWhere |= (
+            (User.firstName ** nameSearch) |
+            (User.lastName ** nameSearch) |
+            (User.username ** nameSearch)
+        )
     
     if category == "instructor":
         userWhere = (User.isFaculty | User.isStaff)
@@ -30,7 +47,16 @@ def searchUsers(query, category=None):
     else:
         userWhere = (User.isStudent)
 
+    fullSearchText = " ".join(splitSearch)
     # Combine into query
-    searchResults = User.select().where(searchWhere, userWhere)
+    searchResults = User.select().where(searchWhere, userWhere).order_by(
+        fn.CONCAT(User.firstName, " ", User.lastName)
+            .contains(fullSearchText)
+            .desc(),
+        User.firstName.startswith(fullSearchText).desc(),
+        User.lastName.startswith(fullSearchText).desc(),
+        User.lastName,
+        User.firstName
+    )
 
     return { user.username : model_to_dict(user) for user in searchResults }
